fee1366109725af4b4ad341a6e48e57aa4e849fb
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-17 17:55:59 +0000

    console.s: Use local instead of global labels.
    No need for these to all be exported as symbols - I just didn't know how
    to make these before.

--- src/console.s
+++ src/console.s
@@ -35,75 +35,75 @@ _puts:
 #   %edx - contains the value to write
 .globl _put32
 _put32:
-    lea     _put32_val, %si
-    lea     _hextab, %bx
-    jmp     _put_8_digits
+    lea     .L_put32_val, %si
+    lea     .L_hextab, %bx
+    jmp     .L_put_8_digits
 
 # Write a uint16_t to the console.
 #   %dx - contains the value to write
 .globl _put16
 _put16:
-    lea     _put16_val, %si
-    lea     _hextab, %bx
-    jmp     _put_4_digits
+    lea     .L_put16_val, %si
+    lea     .L_hextab, %bx
+    jmp     .L_put_4_digits
 
 # Write a uint8_t to the console.
 #   %dl - contains the value to write
 .globl _put8
 _put8:
-    lea     _put8_val, %si
-    lea     _hextab, %bx
-    jmp     _put_2_digits
+    lea     .L_put8_val, %si
+    lea     .L_hextab, %bx
+    jmp     .L_put_2_digits
 
-_put_8_digits:
+.L_put_8_digits:
     mov     %edx, %edi
     shr     $28, %edi
     and     $0x0F, %di
     movb    (%bx,%di), %al
-    movb    %al, _put32_val+0
+    movb    %al, .L_put32_val+0
 
     mov     %edx, %edi
     shr     $24, %edi
     and     $0x0F, %di
     movb    (%bx,%di), %al
-    movb    %al, _put32_val+1
+    movb    %al, .L_put32_val+1
 
     mov     %edx, %edi
     shr     $20, %edi
     and     $0x0F, %di
     movb    (%bx,%di), %al
-    movb    %al, _put32_val+2
+    movb    %al, .L_put32_val+2
 
     mov     %edx, %edi
     shr     $16, %edi
     and     $0x0F, %di
     movb    (%bx,%di), %al
-    movb    %al, _put32_val+3
+    movb    %al, .L_put32_val+3
 
-_put_4_digits:
+.L_put_4_digits:
     mov     %dx, %di
     shr     $12, %di
     and     $0x0F, %di
     movb    (%bx,%di), %al
-    movb    %al, _put16_val+0
+    movb    %al, .L_put16_val+0
 
     mov     %dx, %di
     shr     $8, %di
     and     $0x0F, %di
     movb    (%bx,%di), %al
-    movb    %al, _put16_val+1
+    movb    %al, .L_put16_val+1
 
-_put_2_digits:
+.L_put_2_digits:
     mov     %dx, %di
     shr     $4, %di
     and     $0x0F, %di
     movb    (%bx,%di), %al
-    movb    %al, _put8_val+0
+    movb    %al, .L_put8_val+0
 
     mov     %dx, %di
     and     $0x0F, %di
     movb    (%bx,%di), %al
-    movb    %al, _put8_val+1
+    movb    %al, .L_put8_val+1
 
     jmp     _puts
 
@@ -111,19 +111,19 @@ _put_2_digits:
 # Write a CRLF to the console.
 .globl _putCRLF
 _putCRLF:
-    lea     _putCRLF_string, %si
+    lea     .L_putCRLF_string, %si
     jmp     _puts
 
 
 # --------------------- Data Segment ---------------------
 .data
-_hextab:
+.L_hextab:
     .ascii  "0123456789ABCDEF"
-_put32_val:
+.L_put32_val:
     .ascii  "0000"
-_put16_val:
+.L_put16_val:
     .ascii  "00"
-_put8_val:
+.L_put8_val:
     .asciz  "00"
-_putCRLF_string:
+.L_putCRLF_string:
     .asciz  "\r\n"
