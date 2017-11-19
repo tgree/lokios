7ce715856119e8694230ff26e759f70143394caf
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-19 21:49:32 +0000

    lokios.1: Include _putc in the console function pointers.
    We should probably turn this into a function table in console.s and just
    pass the address of the table to the second stage but...

--- lokios.1/entry.s
+++ lokios.1/entry.s
@@ -5,18 +5,19 @@
 # Dispatch table at address 0x7E10
             .word   .L_mbr_entry
             .word   .L_pxe_entry
+_putc_ptr:  .word   0
 _puts_ptr:  .word   0
 _put16_ptr: .word   0
 _put32_ptr: .word   0
 
+_putc:      jmpw    *_putc_ptr
 _puts:      jmpw    *_puts_ptr
 _put16:     jmpw    *_put16_ptr
 _put32:     jmpw    *_put32_ptr
 
 # Called from lokios.0.  On entry:
-#   AX         - address of the lokios.0 _puts function
-#   BX         - address of the lokios.0 _put16 function
-#   CX         - address of the lokios.0 _put32 function
+#   EAX        - _puts | _putc
+#   EBX        - _put32 | _put16
 #   ES, DS, CS - all 0
 #   SS:SP      - on a valid stack with base 0:0xFC00
 #
@@ -25,9 +26,8 @@ _put32:     jmpw    *_put32_ptr
 #   0xFBFC     - contains original ES:BX (PXENV+ addr)
 .L_mbr_entry:
 .L_pxe_entry:
-    mov     %ax, _puts_ptr
-    mov     %bx, _put16_ptr
-    mov     %cx, _put32_ptr
+    mov     %eax, _putc_ptr
+    mov     %ebx, _put16_ptr
 
     lea     .L_lokios_1_banner, %si
     call    _puts
