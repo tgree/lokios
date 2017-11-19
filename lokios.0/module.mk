498f6ea84cce1e96733dbfcc5725ff7bd23590f1
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-19 06:52:41 +0000

    lokios.0: Rename lokios.ld -> lokios.0.ld.

--- lokios.0/module.mk
+++ lokios.0/module.mk
@@ -9,9 +9,9 @@ CLEAN += \
 	lokios.0/timestamp.o
 
 $(LOKIOS_0_OBJ): ASFLAGS := $(I386_16_ASFLAGS)
-lokios.0/lokios.0.elf: $(LOKIOS_0_OBJ) lokios.0/lokios.ld lokios.0/module.mk
+lokios.0/lokios.0.elf: $(LOKIOS_0_OBJ) lokios.0/lokios.0.ld lokios.0/module.mk
 	@echo '.data' > lokios.0/timestamp.s
 	@echo '.globl _BUILD_TIME' >> lokios.0/timestamp.s
 	@echo '_BUILD_TIME: .asciz "$(NOW)\\r\\n"' >> lokios.0/timestamp.s
 	@$(AS) $(I386_16_ASFLAGS) -o lokios.0/timestamp.o lokios.0/timestamp.s
-	ld -melf_i386 -T lokios.0/lokios.ld -o lokios.0/lokios.0.elf $(LOKIOS_0_OBJ) lokios.0/timestamp.o
+	ld -melf_i386 -T lokios.0/lokios.0.ld -o lokios.0/lokios.0.elf $(LOKIOS_0_OBJ) lokios.0/timestamp.o
