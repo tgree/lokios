cb16707ae89dc072e62ee13685f4d7d0541d7a21
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-19 02:59:12 +0000

    Make: Silence the part of the makefile that builds the timestamp.

--- lokios.0/module.mk
+++ lokios.0/module.mk
@@ -9,8 +9,8 @@ CLEAN += \
 	lokios.0/timestamp.o
 
 lokios.0/lokios.0.elf: $(LOKIOS_0_OBJ) lokios.0/lokios.ld
-	echo '.data' > lokios.0/timestamp.s
-	echo '.globl _BUILD_TIME' >> lokios.0/timestamp.s
-	echo '_BUILD_TIME: .asciz "$(NOW)"' >> lokios.0/timestamp.s
-	$(AS) $(ASFLAGS) -o lokios.0/timestamp.o lokios.0/timestamp.s
+	@echo '.data' > lokios.0/timestamp.s
+	@echo '.globl _BUILD_TIME' >> lokios.0/timestamp.s
+	@echo '_BUILD_TIME: .asciz "$(NOW)"' >> lokios.0/timestamp.s
+	@$(AS) $(ASFLAGS) -o lokios.0/timestamp.o lokios.0/timestamp.s
 	ld -melf_i386 -T lokios.0/lokios.ld -o lokios.0/lokios.0.elf $(LOKIOS_0_OBJ) lokios.0/timestamp.o
