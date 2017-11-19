9f6609fd94e23b1a8146c215685982849fe9a54b
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-19 03:21:31 +0000

    lokios.s: Remove dead test code.

--- lokios.0/lokios.s
+++ lokios.0/lokios.s
@@ -1,9 +1,6 @@
 .code16
 .text
 
-.equiv DUMP_REGS, 0
-.equiv PRINT_TEST, 0
-
 # Entry point for an MBR boot.  On entry:
 #   CS:IP - set to 0:7C00h.
 #   DS    - 0
@@ -39,86 +36,7 @@ _lokios_start_pxe:
     call    _puts
     call    _putCRLF
 
-.if PRINT_TEST
-    mov     $0x789ABCDE, %edx
-    call    _put32
-    call    _putCRLF
-    mov     $0x11CC, %dx
-    call    _put16
-    call    _putCRLF
-.endif
-
-.if DUMP_REGS
-    # Print the contents of some interesting registers.
-    lea     _cr0_text, %si
-    mov     %cr0, %edx
-    call    _dump_reg32
-
-    mov     %sp, %bp
-    lea     _gs_text, %si
-    movw    4(%bp),%dx
-    call    _dump_reg16
-    lea     _gs_text, %si
-    mov     %gs, %dx
-    call    _dump_reg16
-
-    lea     _fs_text, %si
-    movw    6(%bp),%dx
-    call    _dump_reg16
-    lea     _fs_text, %si
-    mov     %fs, %dx
-    call    _dump_reg16
-
-    lea     _es_text, %si
-    movw    8(%bp),%dx
-    call    _dump_reg16
-    lea     _es_text, %si
-    mov     %es,%dx
-    call    _dump_reg16
-
-    lea     _ds_text, %si
-    movw    10(%bp),%dx
-    call    _dump_reg16
-    lea     _ds_text, %si
-    mov     %ds,%dx
-    call    _dump_reg16
-
-    lea     _cs_text, %si
-    mov     %cs, %dx
-    call    _dump_reg16
-
-    lea     _ss_text, %si
-    mov     12(%bp), %dx
-    call    _dump_reg16
-    lea     _ss_text, %si
-    mov     %ss, %dx
-    call    _dump_reg16
-.endif
-
     ret
-
-
-.if DUMP_REGS
-# Print a label and a 32-bit value:
-#   %si  - label
-#   %edx - value
-_dump_reg32:
-    push    %edx
-    call    _puts
-    pop     %edx
-    call    _put32
-    jmp     _putCRLF
-
-# Print a label and a 16-bit value:
-#   %si - label
-#   %dx - value
-_dump_reg16:
-    push    %dx
-    call    _puts
-    pop     %dx
-    call    _put16
-    jmp     _putCRLF
-.endif
 
 
 # --------------------- Data Segment ---------------------
@@ -131,20 +49,3 @@ _mbr_text:
     .asciz  "MBR "
 _pxe_text:
     .asciz  "PXE "
-
-.if DUMP_REGS
-_cr0_text:
-    .asciz  "CR0: "
-_gs_text:
-    .asciz  "GS:  "
-_fs_text:
-    .asciz  "FS:  "
-_es_text:
-    .asciz  "ES:  "
-_ds_text:
-    .asciz  "DS:  "
-_cs_text:
-    .asciz  "CS:  "
-_ss_text:
-    .asciz  "SS:  "
-.endif
