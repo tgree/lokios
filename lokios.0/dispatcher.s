b30fee18ec5eb47cf4a2bef88c2189f8c5085b5c
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-19 07:09:32 +0000

    lokios.0/MBR: Load the first sector of the second-stage bootloader into memory.
    This loads (and jumps to) the entry point of the second-stage
    bootloader from the flat mbr image.

--- lokios.0/dispatcher.s
+++ lokios.0/dispatcher.s
@@ -7,11 +7,37 @@
 #   DL    - contains the drive number for use with INT 13h.
 .globl _dispatch_mbr
 _dispatch_mbr:
+    # Save off DL.
+    pushw   %dx
+
+    # Zero ES because we need it to point to the same place as DS.
+    xor     %dx, %dx
+    mov     %dx, %es
+
     # Print the banners.
     lea     _mbr_text, %si
     call    .L_dispatch_print_banners
 
-    # Bail back to BIOS.
+    # Now we need to load the first sector lokios.1 at 0x7e00.
+    mov     $2, %ah         # opcode
+    mov     $1, %al         # total sector count
+    mov     $0, %ch         # cylinder[7:0]
+    mov     $2, %cl         # cylinder[9:8] | sector (1-based)
+    popw    %dx             # drive number in DL
+    mov     $0, %dh         # head in DH
+    mov     $0x7E00, %bx    # ES:BX = 0:0x7E00
+    int     $0x13
+    jc      .L_mbr_read_failed
+    cmp     $0, %ah
+    jne     .L_mbr_read_failed
+.L_mbr_read_succeeded:
+    lea     _puts, %ax
+    jmp     0x7E10
+
+.L_mbr_read_failed:
+    # Print an error string and bail back to BIOS.
+    lea     _mbr_failed_text, %si
+    call    _puts
     ret
 
 
@@ -59,3 +85,5 @@ _mbr_text:
     .asciz  "MBR "
 _pxe_text:
     .asciz  "PXE "
+_mbr_failed_text:
+    .asciz  "MBR boot failed\r\n"
