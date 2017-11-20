7a0111064b79f24f3ab6403a4650d0943ae9d7a4
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-20 22:01:32 +0000

    lokios.1: Link 16-bit sources in an intermediate output file.
    In the future we are going to support 16-bit sources and then transition
    into 64-bit mode to run the OS.  At a minimum we will have a 64-bit
    trampoline to get into the OS.  64-bit code generation is different than
    16-bit generation and normally the linker doesn't let you mix the two
    (for obvious reasons).
    
    So now what we have to do here is generate an intermediate 16-bit ELF
    file.  Then use objcopy to change the intermediate file's architecture
    to 64-bit (which just changes metadata) and then link it against all of
    the 64-bit object files to make the final "64-bit" ELF file.
    
    In the end it all works and we try to ignore how ugly it is.  Ripe for
    future cleanup, no doubt.

--- lokios.1/module.mk
+++ lokios.1/module.mk
@@ -2,10 +2,16 @@ LOKIOS_1_OBJ := \
 	lokios.1/entry.o    \
 	lokios.1/e820.o     \
 	lokios.1/a20_gate.o
+LOKIOS_1_OBJ_64 :=
 CLEAN += \
-	$(LOKIOS_1_OBJ) \
+	$(LOKIOS_1_OBJ)             \
+	$(LOKIOS_1_OBJ_64)          \
+	lokios.1/lokios.1.16.elf    \
+	lokios.1/lokios.1.16.64.elf \
 	lokios.1/lokios.1.elf
 
 $(LOKIOS_1_OBJ): ASFLAGS := $(I386_16_ASFLAGS)
-lokios.1/lokios.1.elf: $(LOKIOS_1_OBJ) lokios.1/lokios.1.ld
-	ld -melf_i386 -T lokios.1/lokios.1.ld -o lokios.1/lokios.1.elf $(LOKIOS_1_OBJ)
+lokios.1/lokios.1.elf: $(LOKIOS_1_OBJ) $(LOKIOS_1_OBJ_64) lokios.1/lokios.1.16.ld lokios.1/lokios.1.ld
+	ld -i -melf_i386 -T lokios.1/lokios.1.16.ld -o lokios.1/lokios.1.16.elf $(LOKIOS_1_OBJ)
+	objcopy -I elf32-i386 -O elf64-x86-64 lokios.1/lokios.1.16.elf lokios.1/lokios.1.16.64.elf
+	ld -melf_x86_64 -T lokios.1/lokios.1.ld -o lokios.1/lokios.1.elf lokios.1/lokios.1.16.64.elf $(LOKIOS_1_OBJ_64)
