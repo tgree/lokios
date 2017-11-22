e9d46c06ce6584c92ae31a49bc6d92c4d867f34a
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-22 06:31:51 +0000

    lokios.0: Move all source files into bootsector/ subdirectory.
    We're going to merge lokios.0 and lokios.1 into a single lokios.0
    binary.  This sets the stage.

--- lokios.0/module.mk
+++ lokios.0/module.mk
@@ -1,17 +1,17 @@
 LOKIOS_0_OBJ := \
-	lokios.0/dispatcher.o \
-	lokios.0/console.o \
-	lokios.0/entry.o
+	lokios.0/bootsector/dispatcher.o \
+	lokios.0/bootsector/console.o \
+	lokios.0/bootsector/entry.o
 CLEAN += \
 	$(LOKIOS_0_OBJ)       \
 	lokios.0/lokios.0.elf \
-	lokios.0/timestamp.s  \
-	lokios.0/timestamp.o
+	lokios.0/bootsector/timestamp.s  \
+	lokios.0/bootsector/timestamp.o
 
 $(LOKIOS_0_OBJ): ASFLAGS := $(I386_16_ASFLAGS)
 lokios.0/lokios.0.elf: $(LOKIOS_0_OBJ) lokios.0/lokios.0.ld lokios.0/module.mk
-	@echo '.data' > lokios.0/timestamp.s
-	@echo '.globl _BUILD_TIME' >> lokios.0/timestamp.s
-	@echo '_BUILD_TIME: .asciz "$(NOW)\\r\\n"' >> lokios.0/timestamp.s
-	@$(AS) $(I386_16_ASFLAGS) -o lokios.0/timestamp.o lokios.0/timestamp.s
-	ld -melf_i386 -T lokios.0/lokios.0.ld -o lokios.0/lokios.0.elf $(LOKIOS_0_OBJ) lokios.0/timestamp.o
+	@echo '.data' > lokios.0/bootsector/timestamp.s
+	@echo '.globl _BUILD_TIME' >> lokios.0/bootsector/timestamp.s
+	@echo '_BUILD_TIME: .asciz "$(NOW)\\r\\n"' >> lokios.0/bootsector/timestamp.s
+	@$(AS) $(I386_16_ASFLAGS) -o lokios.0/bootsector/timestamp.o lokios.0/bootsector/timestamp.s
+	ld -melf_i386 -T lokios.0/lokios.0.ld -o lokios.0/lokios.0.elf $(LOKIOS_0_OBJ) lokios.0/bootsector/timestamp.o
