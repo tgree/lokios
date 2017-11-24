ddb7468204d4bc1e2255123799d74d4025d40843
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-24 06:34:58 +0000

    unreal_mode: Only put FS into unreal mode.
    The PXE code has to use GS for other things, so don't bother setting it
    up.

--- lokios.0/unreal_mode.s
+++ lokios.0/unreal_mode.s
@@ -16,19 +16,18 @@ _enter_unreal_mode:
     jmp     .L_386_486_bug
 .L_386_486_bug:
 
-    # Load FS and GS with unreal mode.  The $8 below is the offset to the
-    # descriptor in the GDT.  This loads the "hidden part" of the FS and GS
-    # registers with the limits from the GDT entry.
+    # Load FS with unreal mode.  The $8 below is the offset to the descriptor
+    # in the GDT.  This loads the "hidden part" of the FS register with the
+    # limits from the GDT entry.
     mov     $8, %bx
     mov     %bx, %fs
-    mov     %bx, %gs
 
     # Leave protected mode.
     and     $~(1<<0), %eax
     mov     %eax, %cr0
 
-    # Now we reset FS and GS back to 0 so that the linear address is calculated
-    # from a 0 base.  The key here is that in real mode there are no segment
+    # Now we reset FS back to 0 so that the linear address is calculated from a
+    # 0 base.  The key here is that in real mode there are no segment
     # descriptors so the processor doesn't go and reload the "hidden part" of
     # the registers - but it keeps the 4G limit that's already in there!  So we
     # can now use expressions like:
@@ -39,9 +38,8 @@ _enter_unreal_mode:
     # specifying a 32-bit register offset from FS-base).
     mov     $0, %bx
     mov     %bx, %fs
-    mov     %bx, %gs
 
-    # FS and GS are now in unreal mode.
+    # FS is now in unreal mode.
     ret
 
 
