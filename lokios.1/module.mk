922f74a8f724f9d66e9afb339404c540184eb0a2
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-25 17:46:05 +0000

    lokios.1: Wrap the module.mk ld line.

--- lokios.1/module.mk
+++ lokios.1/module.mk
@@ -24,4 +24,7 @@ $(LOKIOS_1_OBJ): CXXFLAGS := $(X86_64_CXXFLAGS)
 lokios.1/main.o: CXXFLAGS := $(X86_64_CXXFLAGS) -Wno-main
 
 lokios.1/lokios.1.elf: $(LOKIOS_1_OBJ) lokios.1/lokios.1.ld lokios.1/module.mk
-	ld -melf_x86_64 -T lokios.1/lokios.1.ld -o lokios.1/lokios.1.elf $(LOKIOS_1_LINK_OBJ)
+	ld -melf_x86_64                       \
+	   -T lokios.1/lokios.1.ld            \
+	   -o lokios.1/lokios.1.elf           \
+	   $(LOKIOS_1_LINK_OBJ)               \
