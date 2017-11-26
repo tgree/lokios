e9275638c65b582430e3c8fea38865950783b857
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-26 05:12:46 +0000

    lokios.1: Add some comments to entry.s.

--- lokios.1/entry.s
+++ lokios.1/entry.s
@@ -1,9 +1,16 @@
 # On entry:
 #   RDI - address of the kernel_args struct
 _entry:
+    # Save off kargs.
     mov     %rdi, kargs
+
+    # Call the _init stuff.
     call    init
+
+    # Call main.
     call    main
+
+    # STOP.
     hlt
 
 .data
