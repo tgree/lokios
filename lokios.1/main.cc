7348903b76042058c5d664483c419e7efa3d20ab
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-26 19:32:38 +0000

    lokios.1: Print out the thread-local-data addresses.

--- lokios.1/main.cc
+++ lokios.1/main.cc
@@ -3,10 +3,21 @@
 #include "e820.h"
 #include "assert.h"
 
+extern uint8_t _tdata_begin[];
+extern uint8_t _tdata_end[];
+extern uint8_t _tbss_begin[];
+extern uint8_t _tbss_end[];
+extern uint8_t _tbss_size[];
+
 int
 main()
 {
     vga.printf("Loki is kickawesome\n");
+    vga.printf("_tdata_begin = %ld\n",(uintptr_t)_tdata_begin);
+    vga.printf("_tdata_end   = %ld\n",(uintptr_t)_tdata_end);
+    vga.printf("_tbss_begin  = %ld\n",(uintptr_t)_tbss_begin);
+    vga.printf("_tbss_end    = %ld\n",(uintptr_t)_tbss_end);
+    vga.printf("_tbss_size   = %ld\n",(uintptr_t)_tbss_size);
 
     try
     {
