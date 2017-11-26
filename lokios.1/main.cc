1688dad74f539ce5755730ddc5ec30152c3b3b92
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-26 19:26:13 +0000

    lokios.1: Add a simple test to demonstrate exception-handling works.
    Despite the simplicity, this does call __cxa_throw() and it does have
    catch blocks installed in the exception tables.  It looks like that's
    even how it gets where it needs to go.

--- lokios.1/main.cc
+++ lokios.1/main.cc
@@ -1,10 +1,30 @@
 #include "kernel_args.h"
 #include "console.h"
 #include "e820.h"
+#include "assert.h"
 
 int
 main()
 {
     vga.printf("Loki is kickawesome\n");
-    vga.printf("Hello %d '%4d' %05d '%-5d'",123,45,67,89);
+
+    try
+    {
+        throw &vga;
+        aborts("throw failed");
+    }
+    catch (int)
+    {
+        aborts("caught int");
+    }
+    catch (console* c)
+    {
+        vga.printf("caught console*\n");
+    }
+    catch (...)
+    {
+        aborts("caught ...");
+    }
+
+    vga.printf("Kernel halting.\n");
 }
