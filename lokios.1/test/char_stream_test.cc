ad25d03ec279f8cdda6794d3bdb2edfb0acc5347
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-27 20:34:29 +0000

    char_stream_test: Improve the unittest framework and do more tests.

--- lokios.1/test/char_stream_test.cc
+++ lokios.1/test/char_stream_test.cc
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BROKEN_TEST 0
+
 void
 panic(const char* s) noexcept
 {
@@ -21,11 +23,71 @@ assert_string_equal(const char* s, const char* e)
     }
 }
 
-int
-main(int argc, const char* argv[])
+static void
+test_fixed_string_stream()
 {
     fixed_string_stream<10> fs;
     fs.printf("Hello, world!");
     assert_string_equal(fs,"Hello, wo");
-    printf("%s passed\n",argv[0]);
+}
+
+template<typename T, size_t N>
+static void run_fmt_tests(T (&tests)[N])
+{
+    for (T& tc : tests)
+    {
+        fixed_string_stream<20> fs;
+        char expected[20];
+
+        fs.printf(tc.fmt,tc.value);
+        snprintf(expected,sizeof(expected),tc.fmt,tc.value);
+        assert_string_equal(fs,expected);
+        assert_string_equal(expected,tc.expected);
+    }
+}
+
+template<typename T>
+struct fmt_test_case
+{
+    T               value;
+    const char*     fmt;
+    const char*     expected;
+};
+static fmt_test_case<unsigned int> uint_tests[] = {
+    {12345,     "%u",  "12345"},
+    {12345,     "%8u", "   12345"},
+    {12345,     "%08u","00012345"},
+    {12345,     "%08d","00012345"},
+};
+static fmt_test_case<int> int_tests[] = {
+    {12345,  "%d", "12345"},
+};
+
+static void
+test_fixed_hex()
+{
+    run_fmt_tests(uint_tests);
+    run_fmt_tests(int_tests);
+}
+
+#define TEST(f) {f,#f}
+struct test_case
+{
+    void (*func)();
+    const char* name;
+};
+static test_case tests[] =
+{
+    TEST(test_fixed_string_stream),
+    TEST(test_fixed_hex),
+};
+
+int
+main(int argc, const char* argv[])
+{
+    for (test_case& tc : tests)
+    {
+        tc.func();
+        printf("%s:%s passed\n",argv[0],tc.name);
+    }
 }
