b541dddc73de684a399f4f43eddbb74c3cb6af77
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-28 23:38:29 +0000

    console_test: Add simple console test module.
    While this technically tests a small subset of the console
    functionality, its actualy purpose is to test multiple test binaries and
    the build infrastructure.

---
+++ lokios.1/test/console_test.cc
@@ -0,0 +1,32 @@
+#include "../console.h"
+#include "../kernel_args.h"
+#include "tmock/tmock.h"
+
+static uint16_t conbuf[80*25];
+static const kernel_args args = {0,(uintptr_t)conbuf};
+const kernel_args* kargs = &args;
+
+void
+panic(const char* s) noexcept
+{
+    abort();
+}
+
+TMOCK_TEST(test_screen_initialized)
+{
+    for (uint16_t v : conbuf)
+        tmock::assert_equiv(v,0x1F20);
+}
+
+TMOCK_TEST(test_putc)
+{
+    tmock::assert_equiv(conbuf[0],0x1F20);
+    vga._putc('T');
+    tmock::assert_equiv(conbuf[0],0x1F00 | 'T');
+}
+
+int
+main(int argc, const char* argv[])
+{
+    return tmock::run_tests(argc,argv);
+}
--- lokios.1/test/module.mk
+++ lokios.1/test/module.mk
@@ -1,7 +1,13 @@
-TESTS += $(TESTDIR)/char_stream_test
+TESTS += $(TESTDIR)/char_stream_test \
+	 $(TESTDIR)/console_test
 
 CHAR_STREAM_TEST_OBJS := \
 	$(MODULE)/char_stream_test.o \
+	$(PARENT)/char_stream.o
+
+CONSOLE_TEST_OBJS := \
+    	$(MODULE)/console_test.o \
+	$(PARENT)/console.o \
 	$(PARENT)/char_stream.o
 
 CLEAN += \
@@ -12,3 +18,6 @@ CLEAN += \
 
 $(TESTDIR)/char_stream_test: $(CHAR_STREAM_TEST_OBJS) $(LTMOCK) $(MODULE)/module.mk
 	$(BUILD_TEST) $(CHAR_STREAM_TEST_OBJS) $(LTMOCK)
+
+$(TESTDIR)/console_test: $(CONSOLE_TEST_OBJS) $(LTMOCK) $(MODULE)/module.mk
+	$(BUILD_TEST) $(CONSOLE_TEST_OBJS) $(LTMOCK)
