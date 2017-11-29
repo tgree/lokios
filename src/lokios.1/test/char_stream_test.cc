cbac09bd2533abb1cfdb7536ddee2ab0f284073e
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-29 07:47:59 +0000

    char_stream_test: Get the host to generate the expected printf string.
    Instead of manually providing a string we might as well just get the
    host system on which we are running the unittest to generate the string
    for us.  This eliminates errors in the test suite and also makes the
    code simpler.

--- src/lokios.1/test/char_stream_test.cc
+++ src/lokios.1/test/char_stream_test.cc
@@ -4,27 +4,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+__attribute__((format(printf,1,2)))
+static void
+test_random_string(const char* fmt, ...)
+{
+    char* buf;
+    va_list ap;
+
+    va_start(ap,fmt);
+    int n = vasprintf(&buf,fmt,ap) + 1;
+    va_end(ap);
+
+    char* buf2 = (char*)malloc(n);
+    string_stream ss(buf2,n);
+    va_start(ap,fmt);
+    ss.vprintf(fmt,ap);
+    va_end(ap);
+
+    tmock::assert_equiv(buf2,buf);
+
+    free(buf2);
+    free(buf);
+}
+
 template<typename T>
 struct fmt_test_case
 {
     T               value;
     const char*     fmt;
-    const char*     expected;
 };
 
 template<typename T, size_t N>
 static void run_fmt_tests(T (&tests)[N])
 {
     for (T& tc : tests)
-    {
-        fixed_string_stream<20> fs;
-        char expected[20];
-
-        fs.printf(tc.fmt,tc.value);
-        snprintf(expected,sizeof(expected),tc.fmt,tc.value);
-        tmock::assert_equiv(fs,expected);
-        tmock::assert_equiv(expected,tc.expected);
-    }
+        test_random_string(tc.fmt,tc.value);
 }
 
 void
@@ -43,13 +57,13 @@ TMOCK_TEST(test_fixed_string_stream)
 TMOCK_TEST(test_fmt_unsigned_int)
 {
     static fmt_test_case<unsigned int> uint_tests[] = {
-        {12345,     "%u",  "12345"},
-        {12345,     "%8u", "   12345"},
-        {12345,     "%08u","00012345"},
-        {12345,     "%08d","00012345"},
-        {0x2468ABCD,"%08X","2468ABCD"},
-        {0x12EF,    "%X",  "12EF"},
-        {0x789AB,   "%08X","000789AB"},
+        {12345,     "%u"  },
+        {12345,     "%8u" },
+        {12345,     "%08u"},
+        {12345,     "%08d"},
+        {0x2468ABCD,"%08X"},
+        {0x12EF,    "%X"  },
+        {0x789AB,   "%08X"},
     };
     run_fmt_tests(uint_tests);
 }
@@ -57,7 +71,7 @@ TMOCK_TEST(test_fmt_unsigned_int)
 TMOCK_TEST(test_fmt_positive_int)
 {
     static fmt_test_case<int> int_tests[] = {
-        {12345,  "%d", "12345"},
+        {12345,"%d"},
     };
     run_fmt_tests(int_tests);
 }
@@ -65,7 +79,7 @@ TMOCK_TEST(test_fmt_positive_int)
 TMOCK_TEST_EXPECT_FAILURE(test_fmt_negative_int)
 {
     static fmt_test_case<int> int_tests[] = {
-        {-12345, "%d", "-12345"},
+        {-12345,"%d"},
     };
     run_fmt_tests(int_tests);
 }
