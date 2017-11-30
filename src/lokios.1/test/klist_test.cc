8710db1fbf65be85e4ae22d0a088238a999b459a
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-11-30 07:53:01 +0000

    klist: Provide klist.h header and test binary.
    This implements a singly-linked list type.

---
+++ src/lokios.1/test/klist_test.cc
@@ -0,0 +1,112 @@
+#include "../klist.h"
+#include "tmock/tmock.h"
+
+struct klinked_object
+{
+    klink   link;
+    int     val;
+
+    constexpr klinked_object(int val):val(val) {}
+};
+
+void
+panic(const char* s) noexcept
+{
+    tmock::abort(s);
+}
+
+TMOCK_TEST(test_unused_klink_works)
+{
+    klink kl;
+}
+
+TMOCK_TEST_EXPECT_FAILURE(test_klink_inuse_assert)
+{
+    klink kl;
+    kl.next = NULL;
+}
+
+TMOCK_TEST(test_empty_klist_works)
+{
+    klist<klinked_object> kl;
+}
+
+TMOCK_TEST_EXPECT_FAILURE(test_klist_head_inuse_assert)
+{
+    klist<klinked_object> kl;
+    kl.head = (klink*)12345;
+}
+
+TMOCK_TEST_EXPECT_FAILURE(test_klist_tail_inuse_assert)
+{
+    klist<klinked_object> kl;
+    kl.tail = (klink*)12345;
+}
+
+TMOCK_TEST(test_klist_push_back_works)
+{
+    klinked_object o1(1);
+    klinked_object o2(2);
+    klinked_object o3(3);
+    klinked_object o4(4);
+    klinked_object o5(5);
+    klist<klinked_object> kl;
+
+    kl.push_back(&o3.link);
+    kl.push_back(&o1.link);
+    kl.push_back(&o4.link);
+
+    tmock::assert_equiv(kl.size(),3);
+
+    kl.pop_all();
+}
+
+TMOCK_TEST(test_klist_rbfl_works)
+{
+    klinked_object o1(1);
+    klinked_object o2(2);
+    klinked_object o3(3);
+    klinked_object o4(4);
+    klinked_object o5(5);
+    klist<klinked_object> kl;
+
+    kl.push_back(&o3.link);
+    kl.push_back(&o1.link);
+    kl.push_back(&o4.link);
+    kl.push_back(&o2.link);
+    kl.push_back(&o5.link);
+
+    static int vals[] = {3,1,4,2,5};
+    int* vp = vals;
+    for (klinked_object& o : klist_elems(kl,link))
+        tmock::assert_equiv(o.val,*vp++);
+    kassert(vp == &vals[5]);
+
+    kl.pop_all();
+}
+
+TMOCK_TEST(test_klist_empty_rbfl_works)
+{
+    klist<klinked_object> kl;
+    size_t n = 0;
+    for (klinked_object& o __attribute__((unused)) : klist_elems(kl,link))
+        ++n;
+    kassert(n == 0);
+}
+
+TMOCK_TEST(test_klist_one_elem_rbfl_works)
+{
+    klist<klinked_object> kl;
+    klinked_object o1(12345);
+    kl.push_back(&o1.link);
+    size_t n = 0;
+    for (klinked_object& o __attribute__((unused)) : klist_elems(kl,link))
+    {
+        kassert(o.val == 12345);
+        ++n;
+    }
+    kassert(n == 1);
+    kl.pop_front();
+}
+
+TMOCK_MAIN();
--- src/lokios.1/test/module.mk
+++ src/lokios.1/test/module.mk
@@ -1,5 +1,6 @@
 TESTS += $(TESTS_DIR)/char_stream_test \
-	 $(TESTS_DIR)/console_test
+	 $(TESTS_DIR)/console_test \
+	 $(TESTS_DIR)/klist_test
 
 CHAR_STREAM_TEST_OBJS := \
 	$(MODULE_BUILD_DIR)/char_stream_test.o \
@@ -12,8 +13,15 @@ CONSOLE_TEST_OBJS := \
 	$(PARENT_BUILD_DIR)/char_stream.o
 -include $(CONSOLE_TEST_OBJS:.o=.d)
 
+KLIST_TEST_OBJS := \
+    	$(MODULE_BUILD_DIR)/klist_test.o
+-include $(KLIST_TEST_OBJS:.o=.d)
+
 $(TESTS_DIR)/char_stream_test: $(CHAR_STREAM_TEST_OBJS) $(LTMOCK) $(MODULE_MK)
 	$(BUILD_TEST) $(CHAR_STREAM_TEST_OBJS) $(LTMOCK)
 
 $(TESTS_DIR)/console_test: $(CONSOLE_TEST_OBJS) $(LTMOCK) $(MODULE_MK)
 	$(BUILD_TEST) $(CONSOLE_TEST_OBJS) $(LTMOCK)
+
+$(TESTS_DIR)/klist_test: $(KLIST_TEST_OBJS) $(LTMOCK) $(MODULE_MK)
+	$(BUILD_TEST) $(KLIST_TEST_OBJS) $(LTMOCK)
