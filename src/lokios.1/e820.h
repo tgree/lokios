4a7fca70c378f96dadfb9a296eb3051b016c5a77
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-01 08:52:19 +0000

    e820: Provide an enum for the type field.

--- src/lokios.1/e820.h
+++ src/lokios.1/e820.h
@@ -5,11 +5,20 @@
 
 namespace kernel
 {
+    enum e820_type : uint32_t
+    {
+        E820_TYPE_RAM              = 1,
+        E820_TYPE_RESERVED         = 2,
+        E820_TYPE_ACPI_RECLAIMABLE = 3,
+        E820_TYPE_ACPI_NVS         = 4,
+        E820_TYPE_BAD_RAM          = 5,
+    };
+
     struct e820_entry
     {
         uint64_t    base;
         uint64_t    len;
-        uint32_t    type;
+        e820_type   type;
         uint32_t    rsrv;
     };
 
