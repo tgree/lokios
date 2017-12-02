613057fb3120ddf3ea206b36d4b76604d5c6e43b
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-02 20:39:40 +0000

    lokios.1: Simplify Makefile usage so we don't need to specify each .o file.
    This makes it easier to add code to the directory - it will just get
    picked up, compiled and linked automatically.

--- src/lokios.1/module.mk
+++ src/lokios.1/module.mk
@@ -2,35 +2,18 @@ SUBMODULES := test
 
 CRTBEGIN_OBJ := $(shell $(CC) $(CFLAGS) -print-file-name=crtbegin.o)
 CRTEND_OBJ   := $(shell $(CC) $(CFLAGS) -print-file-name=crtend.o)
-LSUPCXXX_OBJ := $(shell $(CC) $(CFLAGS) -print-file-name=libsupc++.a)
+LSUPCXX_OBJ  := $(shell $(CC) $(CFLAGS) -print-file-name=libsupc++.a)
 LGCC_EH_OBJ  := $(shell $(CC) $(CFLAGS) -print-file-name=libgcc_eh.a)
 
-LOKIOS_1_OBJ := \
-	$(MODULE_BUILD_DIR)/entry.o		\
-	$(MODULE_BUILD_DIR)/init.o		\
-	$(MODULE_BUILD_DIR)/crti.o		\
-	$(MODULE_BUILD_DIR)/main.o		\
-	$(MODULE_BUILD_DIR)/kassert.o		\
-	$(MODULE_BUILD_DIR)/console.o		\
-	$(MODULE_BUILD_DIR)/char_stream.o	\
-	$(MODULE_BUILD_DIR)/cxxabi.o		\
-	$(MODULE_BUILD_DIR)/libc.o		\
-	$(MODULE_BUILD_DIR)/tls.o		\
-	$(MODULE_BUILD_DIR)/e820.o		\
-	$(MODULE_BUILD_DIR)/sbrk.o		\
-	$(MODULE_BUILD_DIR)/page.o		\
-	$(MODULE_BUILD_DIR)/crtn.o
--include $(LOKIOS_1_OBJ:.o=.d)
+LOKIOS_1_CXX_SRC := $(wildcard $(MODULE_SRC_DIR)/*.cc)
+LOKIOS_1_ASM_SRC := $(wildcard $(MODULE_SRC_DIR)/*.s)
+LOKIOS_1_OBJ := $(LOKIOS_1_CXX_SRC:$(MODULE_SRC_DIR)/%.cc=$(MODULE_BUILD_DIR)/%.o) \
+                $(LOKIOS_1_ASM_SRC:$(MODULE_SRC_DIR)/%.s=$(MODULE_BUILD_DIR)/%.o)
 
-LOKIOS_1_LINK_OBJ := \
-	$(CRTBEGIN_OBJ)	\
-	$(LOKIOS_1_OBJ)	\
-	$(LSUPCXXX_OBJ)	\
-	$(LGCC_EH_OBJ)	\
-	$(CRTEND_OBJ)	\
+-include $(LOKIOS_1_OBJ:.o=.d)
 
 $(MODULE_BUILD_DIR)/lokios.1.elf: LDM  := $(MODULE_BUILD_DIR)/lokios.1.map
 $(MODULE_BUILD_DIR)/lokios.1.elf: LDLD := $(MODULE_SRC_DIR)/lokios.1.ld
 $(MODULE_BUILD_DIR)/lokios.1.elf: $(LOKIOS_1_OBJ) $(MODULE_SRC_DIR)/lokios.1.ld $(MODULE_MK)
 	@echo Linking $@...
-	@ld -melf_x86_64 -Map=$(LDM) -T $(LDLD) -o $@ $(LOKIOS_1_LINK_OBJ)
+	@ld -melf_x86_64 -Map=$(LDM) -T $(LDLD) -o $@ $(CRTBEGIN_OBJ) $(LOKIOS_1_OBJ) $(LSUPCXX_OBJ) $(LGCC_EH_OBJ) $(CRTEND_OBJ)
