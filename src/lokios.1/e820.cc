566b4e6ed09674af10f960ab64dff219a4bcc91e
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-02 09:16:17 +0000

    E820: Do a better job of figuring out what to do with our RAM.

--- src/lokios.1/e820.cc
+++ src/lokios.1/e820.cc
@@ -3,6 +3,7 @@
 #include "console.h"
 #include "sort.h"
 #include "local_vector.h"
+#include "region_set.h"
 #include <string.h>
 
 static const char* e820_type_lut[] =
@@ -15,33 +16,77 @@ static const char* e820_type_lut[] =
     "5 - bad RAM",
 };
 
-static void
-print_e820_entries(const kernel::local_vector<kernel::e820_entry>& entries)
+namespace kernel
 {
-    for (const auto& e : entries)
+#define E820_TYPE_RAM_MASK              (1 << E820_TYPE_RAM)
+#define E820_TYPE_ACPI_RECLAIMABLE_MASK (1 << E820_TYPE_RECLAIMABLE)
+#define E820_TYPE_ACPI_NVS_MASK         (1 << E820_TYPE_ACPI_NVS)
+#define E820_TYPE_BAD_RAM_MASK          (1 << E820_TYPE_BAD_RAM)
+    struct e820_vector : public local_vector<e820_entry>
     {
-        kernel::vga->printf("0x%016lX:0x%016lX %s\n",
-                            e.base,e.base + e.len - 1,
-                            e820_type_lut[e.type < kernel::nelems(e820_type_lut)
-                                          ? e.type : 0]);
-    }
+        void print(char_stream* cs)
+        {
+            for (const auto& e : *this)
+            {
+                const char* type =
+                    e820_type_lut[e.type < nelems(e820_type_lut) ? e.type : 0];
+                cs->printf("0x%016lX:0x%016lX %s\n",
+                           e.base,e.base + e.len - 1,type);
+            }
+        }
+
+        e820_vector(const e820_map* m, e820_entry* storage, size_t capacity,
+                    uint64_t mask):
+            local_vector(storage,capacity)
+        {
+            const e820_entry* e = m->entries;
+            for (size_t i=0; i<m->nentries; ++i, ++e)
+            {
+                if (mask & (1 << e->type))
+                    push_back(*e);
+            }
+            sort::quicksort(*this);
+        }
+    };
 }
 
 void
 kernel::parse_e820_map(const e820_map* m)
 {
-    kernel::vga->printf("E820 entry count: 0x%04X\n",m->nentries);
-    kernel::local_vector<const e820_entry>
-        all_entries(m->entries,m->nentries,m->nentries);
+    // Build a vector of just RAM.
+    e820_entry _ram_entries[m->nentries];
+    e820_vector ram_entries(m,_ram_entries,m->nentries,E820_TYPE_RAM_MASK);
 
-    e820_entry _ram_entries[m->nentries*2];
-    kernel::local_vector<e820_entry> ram_entries(_ram_entries,m->nentries*2);
-    for (const e820_entry& e : all_entries)
+    // Build a region vector, ensuring no zero-length regions.
+    region _ram_regions[m->nentries];
+    local_vector<region> ram_regions(_ram_regions,m->nentries);
+    for (const auto& e : ram_entries)
     {
-        if (e.type == E820_TYPE_RAM)
-            ram_entries.push_back(e);
+        if (e.len)
+            ram_regions.push_back(region{e.base,e.base + e.len - 1});
     }
 
-    kernel::sort::quicksort(ram_entries);
-    print_e820_entries(ram_entries);
+    // Build a vector of non-RAM stuff.
+    size_t nnonram_entries = m->nentries - ram_entries.size();
+    e820_entry _nonram_entries[nnonram_entries];
+    e820_vector nonram_entries(m,_nonram_entries,nnonram_entries,
+                               ~E820_TYPE_RAM_MASK);
+    kassert(nonram_entries.size() == nnonram_entries);
+
+    // Dump the tables.
+    vga->printf("Non-RAM entries:\n");
+    nonram_entries.print(vga);
+    vga->printf("RAM entries:\n");
+    ram_entries.print(vga);
+    vga->printf("E820 entry count: %d\n",m->nentries);
+
+    // Remove the non-RAM regions from the RAM regions in case BIOS gave us
+    // some inconsistent overlapping stuff.
+    for (const auto& e : nonram_entries)
+        region_remove(ram_regions,e.base,e.base + e.len - 1);
+
+    // Dump it.
+    vga->printf("Reduced RAM regions:\n");
+    for (const auto& r : ram_regions)
+        vga->printf("0x%016lX:0x%016lX\n",r.first,r.last);
 }
