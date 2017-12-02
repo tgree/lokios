280e380ab1bc1a9bea05ab4d65532fd9b8d607a4
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-02 06:37:41 +0000

    local_vector: Provide insert/erase methods and make them work like std::vector<>.

--- src/lokios.1/local_vector.h
+++ src/lokios.1/local_vector.h
@@ -2,12 +2,16 @@
 #define __KERNEL_LOCAL_VECTOR_H
 
 #include "kassert.h"
+#include <stddef.h>
 
 namespace kernel
 {
     template<typename T>
     struct local_vector
     {
+        typedef T   value_type;
+        typedef T*  iterator;
+
         T*              _elems;
         size_t          _size;
         const size_t    _capacity;
@@ -23,26 +27,35 @@ namespace kernel
             return _elems[index];
         }
 
-        inline void insert_at_index(const T& val, size_t index)
-        {
-            kassert(!full());
-            kassert(index < size() + 1);
-            for (size_t i=size(); i != index; --i)
-                _elems[i] = _elems[i-1];
-            _elems[index] = val;
-            ++_size;
-        }
-
         inline void push_back(const T& val)
         {
             kassert(!full());
             _elems[_size++] = val;
         }
 
-        inline T* begin()             {return _elems;}
-        inline const T* begin() const {return _elems;}
-        inline T* end()               {return _elems + _size;}
-        inline const T* end() const   {return _elems + _size;}
+        inline void insert(iterator pos, const T& val)
+        {
+            kassert(!full());
+            kassert(pos <= end());
+            for (iterator i = end(); i != pos; --i)
+                *i = *(i-1);
+            *pos = val;
+            ++_size;
+        }
+
+        inline iterator erase(iterator at)
+        {
+            kassert(at < end());
+            for (iterator i = at; i + 1 != end(); ++i)
+                *i = *(i+1);
+            --_size;
+            return at;
+        }
+
+        inline iterator begin()             {return _elems;}
+        inline const iterator begin() const {return _elems;}
+        inline iterator end()               {return _elems + _size;}
+        inline const iterator end() const   {return _elems + _size;}
 
         inline local_vector(T* _elems, size_t _capacity, size_t _size=0):
             _elems(_elems),_size(_size),_capacity(_capacity)
