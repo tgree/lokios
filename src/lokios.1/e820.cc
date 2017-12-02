f4b5ea0fb86cf4b751fd3cb8e42dc7f87d0140fd
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-02 22:50:29 +0000

    E820: Remove dead code.

--- src/lokios.1/e820.cc
+++ src/lokios.1/e820.cc
@@ -7,41 +7,6 @@
 #include "page.h"
 #include <string.h>
 
-static const char* e820_type_lut[] =
-{
-    "reserved",
-    "1 - RAM",
-    "2 - reserved",
-    "3 - ACPI reclaimable",
-    "4 - ACPI NVS",
-    "5 - bad RAM",
-};
-
-namespace kernel
-{
-    struct e820_vector : public local_vector<e820_entry>
-    {
-        void print(char_stream* cs)
-        {
-            for (const auto& e : *this)
-            {
-                const char* type =
-                    e820_type_lut[e.type < nelems(e820_type_lut) ? e.type : 0];
-                cs->printf("0x%016lX:0x%016lX %s\n",
-                           e.base,e.base + e.len - 1,type);
-            }
-        }
-
-        e820_vector(const e820_map* m, e820_entry* storage, size_t capacity,
-                    uint64_t mask):
-            local_vector(storage,capacity)
-        {
-            get_e820_map(m,*this,mask);
-            sort::quicksort(*this);
-        }
-    };
-}
-
 void
 kernel::parse_e820_map(const e820_map* m)
 {
