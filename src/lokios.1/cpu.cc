c048fd493e93adbd7111f7a5ef6e71bc24ca250f
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-07 09:17:47 +0000

    cpu: Add cpu code to set up the various descriptor and interrupt tables.
    With this infrastructure we can now actually install an interrupt
    handler in some vector and invoke it via an INT xxh instruction.

---
+++ src/lokios.1/cpu.cc
@@ -0,0 +1,46 @@
+#include "cpu.h"
+#include "x86.h"
+#include "mm/page.h"
+
+#define MAX_CPUS    1
+
+static kernel::cpu* cpus[MAX_CPUS];
+
+kernel::cpu*
+kernel::init_main_cpu()
+{
+    cpu* c = new cpu;
+
+    // Start by setting up the GDT.
+    c->gdt[0] = 0x0000000000000000;     // Unused/reserved.
+    c->gdt[1] = 0x00209A0000000000;     // Code descriptor
+    c->gdt[2] = 0x0000920000000000;     // Data descriptor
+    c->gdt[4] = TSS_DESC_0((uint64_t)&c->tss,sizeof(c->tss));
+    c->gdt[5] = TSS_DESC_1((uint64_t)&c->tss,sizeof(c->tss));
+    lgdt((uint64_t)c->gdt,sizeof(c->gdt)-1);
+
+    // Fill in the TSS and load the TR.
+    memset(&c->tss,0,sizeof(c->tss));
+    c->tss.iomap_base = sizeof(c->tss);
+    for (size_t i=1; i<nelems(c->tss.ist); ++i)
+        c->tss.ist[i] = (uint64_t)page_zalloc() + PAGE_SIZE - 32;
+    ltr(4*sizeof(c->gdt[0]));
+
+    // Load the IDT.
+    memset(&c->idt,0,sizeof(c->idt));
+    lidt((uint64_t)c->idt,sizeof(c->idt)-1);
+
+    // Done.
+    cpus[0] = c;
+    return c;
+}
+
+void
+kernel::cpu::register_exception_vector(size_t v, void (*handler)())
+{
+    uint64_t p = (uint64_t)handler;
+    idt[v].hi = ((p >> 32) & 0x00000000FFFFFFFF);
+    idt[v].lo = ((p << 32) & 0xFFFF000000000000) |
+                             0x00008E0100080000  |
+                ((p >>  0) & 0x000000000000FFFF);
+}
---
+++ src/lokios.1/cpu.h
@@ -0,0 +1,53 @@
+#ifndef __KERNEL_CPU_H
+#define __KERNEL_CPU_H
+
+#include "hdr/compiler.h"
+#include "kernel/kassert.h"
+#include <stdint.h>
+#include <stddef.h>
+
+namespace kernel
+{
+    struct tss64
+    {
+        uint32_t    rsrv0;
+        uint64_t    rsp[3];
+        uint64_t    ist[8];     // IST0 is reserved.
+        uint8_t     rsrv1[10];
+        uint16_t    iomap_base;
+    } __PACKED__;
+    KASSERT(sizeof(tss64) == 104);
+
+#define TSS_DESC_0(base,limit) \
+    ((((base)  << 32) & 0xFF00000000000000) | \
+     (((limit) << 32) & 0x000F000000000000) | \
+                        0x0000890000000000  | \
+     (((base)  << 16) & 0x000000FFFFFF0000) | \
+     (((limit) <<  0) & 0x000000000000FFFF))
+#define TSS_DESC_1(base,limit) \
+    (((base) >> 32) & 0x00000000FFFFFFFF)
+
+    struct cpu
+    {
+        uint64_t    gdt[6];
+        uint64_t    rsrv1[2];
+
+        tss64       tss;
+        uint64_t    rsrv2;
+
+        struct
+        {
+            uint64_t    lo;
+            uint64_t    hi;
+        } idt[128];
+
+        uint8_t     rsrv3[1872];
+
+        void register_exception_vector(size_t v, void (*handler)());
+    };
+    KASSERT(sizeof(cpu) == 4096);
+
+    cpu* init_main_cpu();
+}
+
+#endif /* __KERNEL_CPU_H */
