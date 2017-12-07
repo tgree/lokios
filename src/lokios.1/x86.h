998703559a7c9abdd5e33e9085d067579e52e9eb
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-07 09:11:49 +0000

    x86.h: Add inlines to load various descriptor tables and segment registers.

--- src/lokios.1/x86.h
+++ src/lokios.1/x86.h
@@ -1,6 +1,8 @@
 #ifndef __KERNEL_X86_H
 #define __KERNEL_X86_H
 
+#include "hdr/compiler.h"
+
 static inline uint64_t mfcr3()
 {
     uint64_t rc;
@@ -23,6 +25,38 @@ static inline uint64_t mfcr4()
 static inline void mtcr4(uint64_t val)
 {
     asm ("mov %0, %%cr4" : : "r"(val));
+}
+
+static inline void lgdt(uint64_t base, uint16_t limit)
+{
+    struct __PACKED__
+    {
+        uint16_t    limit;
+        uint64_t    base;
+    } gdtpd = {limit,base};
+    KASSERT(sizeof(gdtpd) == 10);
+    asm ("lgdt %0" : : "m"(gdtpd));
+}
+
+static inline void lidt(uint64_t base, uint16_t limit)
+{
+    struct __PACKED__
+    {
+        uint16_t    limit;
+        uint64_t    base;
+    } idtpd = {limit,base};
+    KASSERT(sizeof(idtpd) == 10);
+    asm ("lidt %0" : : "m"(idtpd));
+}
+
+static inline void ltr(uint16_t val)
+{
+    asm ("ltr %0" : : "r"(val));
+}
+
+static inline void int32()
+{
+    asm ("int $32");
 }
 
 #endif /* __KERNEL_X86_H */
