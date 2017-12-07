78e0483759f4060cd9e34fd29d1bf33b5750c55f
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-07 09:10:36 +0000

    compiler.h: Add __PACKED__ and __ALIGNED__ macros.

--- src/hdr/compiler.h
+++ src/hdr/compiler.h
@@ -1,6 +1,8 @@
 #ifndef __LOKIOS_COMPILER_H
 #define __LOKIOS_COMPILER_H
 
-#define __UNUSED__ __attribute__((unused))
+#define __UNUSED__      __attribute__((unused))
+#define __PACKED__      __attribute__((packed))
+#define __ALIGNED__(n)  __attribute__((aligned(n)))
 
 #endif /* __LOKIOS_COMPILER_H */
