05d022f507a4a93ad166b12636abec0924873137
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-08 07:54:56 +0000

    main.cc: Print out less crap (no more TLS addresses).

--- src/lokios.1/main.cc
+++ src/lokios.1/main.cc
@@ -2,23 +2,12 @@
 #include "console.h"
 #include "kassert.h"
 
-extern uint8_t _tdata_begin[];
-extern uint8_t _tdata_end[];
-extern uint8_t _tbss_begin[];
-extern uint8_t _tbss_end[];
-extern uint8_t _tbss_size[];
-
 const kernel::kernel_args* kernel::kargs;
 
 int
 main()
 {
     kernel::vga->printf("Loki is kickawesome\n");
-    kernel::vga->printf("_tdata_begin = 0x%016lX\n",(uintptr_t)_tdata_begin);
-    kernel::vga->printf("_tdata_end   = 0x%016lX\n",(uintptr_t)_tdata_end);
-    kernel::vga->printf("_tbss_begin  = 0x%016lX\n",(uintptr_t)_tbss_begin);
-    kernel::vga->printf("_tbss_end    = 0x%016lX\n",(uintptr_t)_tbss_end);
-    kernel::vga->printf("_tbss_size   = 0x%016lX\n",(uintptr_t)_tbss_size);
 
     try
     {
