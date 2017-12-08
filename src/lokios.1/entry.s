b82b32691aa05f4130744b500171528d817463d1
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-08 07:57:30 +0000

    entry.s: Remove TLS setup assembly code.
    We can now do this much more easily when creating a task thread in C++.

--- src/lokios.1/entry.s
+++ src/lokios.1/entry.s
@@ -4,34 +4,6 @@ _entry:
     # Populate kernel::args.
     mov     %rdi, _ZN6kernel5kargsE
 
-    # Clear the TLS BSS.
-    mov     $_tbss_size, %ecx
-    test    %ecx, %ecx
-    je      .L_zero_loop_done
-    mov     $-1, %rbx
-    xor     %rax, %rax
-.L_zero_loop:
-    movb    %al, %fs:(%rbx)
-    dec     %rbx
-    loop    .L_zero_loop
-.L_zero_loop_done:
-
-    # Copy in the TLS TDATA.
-    lea     _tdata_begin, %esi
-    mov     $_tdata_size, %rcx
-    test    %rcx, %rcx
-    je      .L_copy_loop_done
-    xor     %rbx, %rbx
-    sub     $_tbss_size, %rbx
-    sub     %rcx, %rbx # rbx = -_tbss_size - _tdata_size
-.L_copy_loop:
-    movb    (%esi), %al
-    movb    %al, %fs:(%rbx)
-    inc     %esi
-    inc     %rbx
-    loop    .L_copy_loop
-.L_copy_loop_done:
-
     # Enable SSE since the stupid compiler is generating %xmm register
     # references and I can't turn it off because then stdint.h doesn't work.
     mov     %cr0, %rax
