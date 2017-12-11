1e0ba8452b415ca3dc512dac0322513ae84750d3
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-11 02:06:26 +0000

    local_vector: Provide a constexpr constructor.

--- src/lokios.1/k++/local_vector.h
+++ src/lokios.1/k++/local_vector.h
@@ -8,6 +8,8 @@ namespace kernel
     template<typename T>
     struct local_vector : public vector_base<T>
     {
+        constexpr local_vector(T* _elems, size_t _capacity):
+            vector_base<T>(_elems,_capacity) {}
         inline local_vector(T* _elems, size_t _capacity, size_t _size):
             vector_base<T>(_elems,_capacity,_size) {}
     };
