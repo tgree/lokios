65aa2a55ef33036d45fadaada47bc9ef1d02afae
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-13 07:27:54 +0000

    checksum: Add checksum.h to hold standard templated checksums.

---
+++ src/lokios.1/k++/checksum.h
@@ -0,0 +1,20 @@
+#ifndef __KERNEL_CHECKSUM_H
+#define __KERNEL_CHECKSUM_H
+
+#include "kernel/kassert.h"
+
+namespace kernel
+{
+    template<typename T>
+    T checksum(const void* _base, size_t len)
+    {
+        kassert((len % sizeof(T)) == 0);
+        const T* p = (const T*)_base;
+        T sum = 0;
+        while (len--)
+            sum += *p++;
+        return sum;
+    }
+}
+
+#endif /* __KERNEL_CHECKSUM_H */
