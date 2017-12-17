8c5e7c4bf8941dc24395f9dbc4c58944997ce14b
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-17 22:54:39 +0000

    compiler.h: Add __PRINTF__ attribute macro.

--- src/hdr/compiler.h
+++ src/hdr/compiler.h
@@ -4,5 +4,6 @@
 #define __UNUSED__      __attribute__((unused))
 #define __PACKED__      __attribute__((packed))
 #define __ALIGNED__(n)  __attribute__((aligned(n)))
+#define __PRINTF__(a,b) __attribute__((format(printf,a,b)))
 
 #endif /* __LOKIOS_COMPILER_H */
