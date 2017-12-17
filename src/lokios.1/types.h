84ed29943190ea812e2c5c18c6a3d90f549d61ba
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-17 10:35:40 +0000

    types: Provide the fat_register<> template class.
    This class allows us to much more easily define register structs that
    have a bunch of padding between the registers (such as for the various
    APICs in the system).

--- src/lokios.1/types.h
+++ src/lokios.1/types.h
@@ -1,6 +1,8 @@
 #ifndef __KERNEL_TYPES_H
 #define __KERNEL_TYPES_H
 
+#include <stddef.h>
+
 namespace kernel
 {
     struct non_copyable
@@ -9,6 +11,16 @@ namespace kernel
 
         non_copyable() = default;
         non_copyable(const non_copyable&) = delete;
+    };
+
+    template<size_t Width, typename T>
+    struct fat_register
+    {
+        volatile T  data;
+        const char  padding[Width - sizeof(T)];
+
+        void operator=(T v)    {data = v;}
+        operator volatile T&() {return data;}
     };
 }
 
