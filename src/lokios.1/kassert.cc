d05e452d76f4011a43f7edde810247516d53bc99
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-18 00:45:27 +0000

    kassert: Add support for emitting assertions on the serial port.

--- src/lokios.1/kassert.cc
+++ src/lokios.1/kassert.cc
@@ -1,12 +1,14 @@
 #include "kassert.h"
 #include "kernel_args.h"
 #include "vga.h"
+#include "serial.h"
 #include <stdlib.h>
 
 void
 kernel::halt() noexcept
 {
     vga_write(74,24,"Halted");
+    serial_write("Halted\n");
     for (;;)
         asm ("hlt;");
 }
@@ -16,6 +18,9 @@ abort()
 {
     // Hilite the entire screen red.
     kernel::vga_set_flags(0x4F00);
+
+    // Let the serial console know.
+    kernel::serial_write("ABORT\n");
 
     // Stop.
     kernel::halt();
@@ -28,5 +33,8 @@ kernel::panic(const char* s) noexcept
     // screen.
     kernel::vga_write(0,0,"Kernel panic");
     kernel::vga_write(0,1,s);
+    kernel::serial_write("Kernel panic\n");
+    kernel::serial_write(s);
+    kernel::serial_write("\n");
     kernel::halt();
 }
