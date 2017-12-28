12d70cc2624addb4554605591f3b1ffc23fcd509
Terry Greeniaus <terrygreeniaus@gmail.com>
2017-12-28 06:06:58 +0000

    pic: Consult the ACPI tables to see if we need to disable the PIC.

--- src/lokios.1/interrupts/pic.cc
+++ src/lokios.1/interrupts/pic.cc
@@ -1,9 +1,24 @@
 #include "pic.h"
+#include "acpi/tables.h"
+#include "kernel/console.h"
 #include "kernel/x86.h"
+
+using kernel::console::printf;
 
 void
 kernel::init_pic()
 {
+    // Check the MADT table to see if we have a PIC.
+    const sdt_header* h = find_acpi_table(MADT_SIG);
+    kassert(h != NULL);
+    const madt_table* madt = (const madt_table*)h;
+    if (!(madt->flags & (1 << 0)))
+    {
+        printf("MADT indicates no PIC present.\n");
+        return;
+    }
+    printf("MADT indicates PIC present, masking interrupts.\n");
+
     // Mask all interrupts in the master PIC.
     outb(0xFF,0x21);
 
