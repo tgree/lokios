e97b144f708b1ce747a7b0f5ae4c4247e639fde5
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-02-02 07:56:28 +0000

    e1000: Add link to hardware spec.

--- src/lokios.1/net/e1000/e1000.cc
+++ src/lokios.1/net/e1000/e1000.cc
@@ -1,3 +1,8 @@
+/*
+ * Driver for the Intel 8254x family of Ethernet devices (also known as e1000).
+ * Reference:
+ *  //www.intel.com/content/dam/doc/manual/pci-pci-x-family-gbe-controllers-software-dev-manual.pdf
+ */
 #include "e1000.h"
 
 e1000::dev::dev(const kernel::pci::dev* pd, const e1000::driver* owner):
