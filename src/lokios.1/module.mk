00c7a1ac1a761b3b144e3d757c8f58a9997cd841
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-02-02 07:33:25 +0000

    lokios.1: Add ability to link in driver archives.
    We define a driver to have a name (i.e. "e1000") and provide a symbol
    named the driver name suffixed with _driver (i.e. "e1000_driver").  That
    symbol is used to pull in the driver archive; typically it will be a
    kernel::pci::driver global that knows how to match against a PCI device
    and instantiate a kernel::pci::dev to drive it.

--- src/lokios.1/module.mk
+++ src/lokios.1/module.mk
@@ -9,7 +9,9 @@ LOKIOS_1_CXX_SRC := $(wildcard $(MODULE_SRC_DIR)/*.cc)
 LOKIOS_1_ASM_SRC := $(wildcard $(MODULE_SRC_DIR)/*.s)
 LOKIOS_1_OBJ := $(LOKIOS_1_CXX_SRC:$(MODULE_SRC_DIR)/%.cc=$(MODULE_BUILD_DIR)/%.o) \
                 $(LOKIOS_1_ASM_SRC:$(MODULE_SRC_DIR)/%.s=$(MODULE_BUILD_DIR)/%.o)
-LOKIOS_1_LIB := mm.a acpi.a interrupts.a pci.a platform.a k++.a
+LOKIOS_1_DRIVERS :=
+LOKIOS_1_LIB := mm.a acpi.a interrupts.a pci.a platform.a k++.a \
+                $(LOKIOS_1_DRIVERS:%=%.a)
 
 -include $(LOKIOS_1_OBJ:.o=.d)
 
@@ -17,7 +19,7 @@ $(MODULE_BUILD_DIR)/lokios.1.elf: LDM  := $(MODULE_BUILD_DIR)/lokios.1.map
 $(MODULE_BUILD_DIR)/lokios.1.elf: LDLD := $(MODULE_SRC_DIR)/lokios.1.ld
 $(MODULE_BUILD_DIR)/lokios.1.elf: $(LOKIOS_1_OBJ) $(LOKIOS_1_LIB:%=$(LIB_DIR)/%) $(MODULE_SRC_DIR)/lokios.1.ld $(MODULE_MK)
 	@echo Linking $@...
-	@ld -melf_x86_64 -Map=$(LDM) -T $(LDLD) -o $@ \
+	@ld -melf_x86_64 -Map=$(LDM) -T $(LDLD) $(LOKIOS_1_DRIVERS:%=--require-defined=%_driver) -o $@ \
 	    	$(CRTBEGIN_OBJ) \
 		$(LOKIOS_1_OBJ) \
 		-\(             \
