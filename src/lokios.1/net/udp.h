8d50d37980a4be8bc84c403f787ecf8b27469524
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-02-12 07:41:42 +0000

    net: Add udp struct definitions.

---
+++ src/lokios.1/net/udp.h
@@ -0,0 +1,20 @@
+#ifndef __KERNEL_NET_UDP_H
+#define __KERNEL_NET_UDP_H
+
+#include "hdr/compiler.h"
+#include "kernel/kassert.h"
+#include <stdint.h>
+
+namespace udp
+{
+    struct header
+    {
+        be_uint16_t src_port;
+        be_uint16_t dst_port;
+        be_uint16_t len;
+        be_uint16_t checksum;
+    } __PACKED__;
+    KASSERT(sizeof(header) == 8);
+}
+
+#endif /* __KERNEL_NET_UDP_H */
