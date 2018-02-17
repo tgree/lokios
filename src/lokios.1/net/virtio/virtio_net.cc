b57c409cacd47d61529fe9d1efe368bda0e14533
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-02-17 04:57:51 +0000

    virtio_net: Don't forget to activate the interface.

--- src/lokios.1/net/virtio/virtio_net.cc
+++ src/lokios.1/net/virtio/virtio_net.cc
@@ -379,6 +379,7 @@ virtio_net::dev::handle_cq_completion(uint16_t index)
 
             // Create the interface.
             intf = new virtio_net::interface(this);
+            intf->activate();
 
             state = VN_STATE_DONE;
         }
