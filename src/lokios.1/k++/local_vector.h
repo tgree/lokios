8f16a0ced0ec2b7a2734ea26050f5aab49d1b997
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-06-14 21:46:38 +0000

    local_vector: Introduce sbrk_vector<> that allocates backing via sbrk.

--- src/lokios.1/k++/local_vector.h
+++ src/lokios.1/k++/local_vector.h
@@ -2,6 +2,7 @@
 #define __KERNEL_LOCAL_VECTOR_H
 
 #include "vector_base.h"
+#include "../mm/sbrk.h"
 
 namespace kernel
 {
@@ -12,6 +13,16 @@ namespace kernel
             vector_base<T>(_elems,_capacity) {}
         inline local_vector(T* _elems, size_t _capacity, size_t _size):
             vector_base<T>(_elems,_capacity,_size) {}
+    };
+
+    template<typename T>
+    struct sbrk_vector : public local_vector<T>
+    {
+        inline sbrk_vector(size_t bytelen):
+            local_vector<T>((T*)phys_to_virt(sbrk(bytelen)),
+                            bytelen/sizeof(T))
+        {
+        }
     };
 }
 
