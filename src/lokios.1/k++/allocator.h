479d27c9af49071ccc58234c6c1f7610837ebda4
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-06-18 00:36:34 +0000

    allocator: Add raii_block<> template type.
    This allows allocating and freeing a block of memory using a specified
    allocator with RAII semantics so it will be automatically cleaned up on
    error paths.

--- src/lokios.1/k++/allocator.h
+++ src/lokios.1/k++/allocator.h
@@ -1,6 +1,8 @@
 #ifndef __KERNEL_ALLOCATOR_H
 #define __KERNEL_ALLOCATOR_H
 
+#include "kmath.h"
+
 namespace kernel
 {
     template<size_t Len>
@@ -11,6 +13,32 @@ namespace kernel
         uint64_t                backing[ceil_div(Len,sizeof(uint64_t))];
         inline void* alloc()      {return backing;}
         inline void free(void* p) {kassert(p == backing);}
+    };
+
+    template<typename Allocator>
+    struct raii_block
+    {
+        Allocator&  allocator;
+        void*       addr;
+
+        inline raii_block(Allocator& allocator):
+            allocator(allocator),
+            addr(allocator.alloc())
+        {
+        }
+
+        inline raii_block(raii_block&& other):
+            allocator(other.allocator),
+            addr(other.addr)
+        {
+            kassert(allocator.movable);
+            other.addr = allocator.alloc();
+        }
+
+        inline ~raii_block()
+        {
+            allocator.free(addr);
+        }
     };
 }
 
