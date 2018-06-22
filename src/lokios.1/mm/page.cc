1da247462f6edcf06baa0497ebe03ca2a4d4b435
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-06-22 02:46:20 +0000

    page: Use buddy_allocator instead of a simple list to track free pages.
    This allows higher-order (multi-page) allocations.  It also allows
    aligned allocations of higher order which will be very useful for
    drivers.

--- src/lokios.1/mm/page.cc
+++ src/lokios.1/mm/page.cc
@@ -1,134 +1,66 @@
 #include "page.h"
-#include "mm.h"
-#include "sbrk.h"
+#include "buddy_allocator.h"
 #include "../spinlock.h"
 #include "../task.h"
 #include "../cpu.h"
 #include "k++/local_vector.h"
-#include <new>
+#include "k++/deferred_global.h"
 
-struct page
-{
-    kernel::klink   link;
-    char            data[4084];
-};
-KASSERT(sizeof(page) == PAGE_SIZE);
-
-static kernel::spinlock     free_page_lock;
-static kernel::klist<page>  free_page_list;
+static kernel::spinlock                                 buddy_pages_lock;
+static kernel::deferred_global<kernel::buddy_allocator> buddy_pages;
 
 void*
 kernel::page_alloc()
 {
-    page* p;
-
-    with (free_page_lock)
+    with (buddy_pages_lock)
     {
-        kassert(!free_page_list.empty());
-        p = klist_front(free_page_list,link);
-        free_page_list.pop_front();
+        return phys_to_virt(buddy_pages->alloc_pages(0));
     }
-
-    p->~page();
-    return p;
 }
 
 void
-kernel::page_free(void* _p)
+kernel::page_free(void* p)
 {
-    kassert(((uintptr_t)_p & PAGE_OFFSET_MASK) == 0);
-    page* p = new(_p) page;
-    with (free_page_lock)
+    kassert(((uintptr_t)p & PAGE_OFFSET_MASK) == 0);
+    with (buddy_pages_lock)
     {
-        free_page_list.push_front(&p->link);
-    }
-}
-
-static void
-page_add(void* _p)
-{
-    kernel::kassert(((uintptr_t)_p & PAGE_OFFSET_MASK) == 0);
-    page* p = new(_p) page;
-    with (free_page_lock)
-    {
-        free_page_list.push_back(&p->link);
+        buddy_pages->free_pages(virt_to_phys(p),0);
     }
 }
 
 size_t
 kernel::page_count_free()
 {
-    with (free_page_lock)
-    {
-        return free_page_list.size();
-    }
-}
-
-// Given a list of regions in some container, add them to the free page list.
-template<typename C>
-static void
-populate_pages(const C& c, kernel::klist<page>& fpl)
-{
-    for (const kernel::region& r : c)
-    {
-        uintptr_t begin = r.first;
-        uintptr_t end   = r.last + 1;
-        if (begin & PAGE_OFFSET_MASK)
-            begin = (begin + PAGE_OFFSET_MASK) & PAGE_PFN_MASK;
-        if (end & PAGE_OFFSET_MASK)
-            end &= PAGE_PFN_MASK;
-        if (end <= begin)
-            continue;
-
-        uint64_t begin_pfn = begin/PAGE_SIZE;
-        uint64_t end_pfn   = end/PAGE_SIZE;
-        for (size_t pfn = begin_pfn; pfn != end_pfn; ++pfn)
-        {
-            void* _p = kernel::phys_to_virt(pfn*PAGE_SIZE);
-            kernel::kassert(((uintptr_t)_p & PAGE_OFFSET_MASK) == 0);
-            page* p = new(_p) page;
-            fpl.push_back(&p->link);
-        }
-    }
+    return buddy_pages->nfree_pages;
 }
 
 void
 kernel::page_preinit(const e820_map* m, uint64_t top_addr)
 {
-    // Initialize all of the pages mapped by the bootloader.  This will be the
-    // first 8M of memory, mapped in the 0xFFFF800000000000 region.
+    // Populate the buddy allocator with the free bootloader-mapped pages that
+    // come following the sbrk region.
 
-    // Find the initial sbrk position.
-    dma_addr64 initial_sbrk = sbrk(0);
-
-    // Leave at least 1M of space in sbrk for early libsupc++ malloc() calls.
-    // We round this up to a 2M hugepage boundary.
-    dma_addr64 sbrk_limit = initial_sbrk + 1024*1024;
-    sbrk_limit = (sbrk_limit + 0x001FFFFF) & ~0x001FFFFFUL;
-    kassert(sbrk_limit < top_addr);
-    set_sbrk_limit(sbrk_limit);
-
-    // We need 8K to back our vector<> objects.
-    kassert(sbrk_limit + 2*4096 <= top_addr);
-
-    // Parse the usable RAM regions out of the E820 map.
+    // Parse the usable RAM regions out of the E820 map and subtract the
+    // unusable ones for safety.
     sbrk_vector<region> usable_regions;
-    get_e820_regions(m,usable_regions,E820_TYPE_RAM_MASK);
-
-    // Parse the unusable regions out of the E820 map.
     sbrk_vector<region> unusable_regions;
+    get_e820_regions(m,usable_regions,E820_TYPE_RAM_MASK);
     get_e820_regions(m,unusable_regions,~E820_TYPE_RAM_MASK);
-
-    // Remove all unusable regions in case BIOS gave us overlap.
     regions_remove(usable_regions,unusable_regions);
-
-    // Remove everything above the top bootloader address.
-    region_remove(usable_regions,top_addr,0xFFFFFFFFFFFFFFFF);
 
     // Remove everything below the sbrk limit.  This includes all of the
     // kernel image, all of BIOS stuff in low memory and anything that gets
     // allocated out of the sbrk pool.
     region_remove(usable_regions,0,(uintptr_t)get_sbrk_limit()-1);
+
+    // What we have now is the working set of free pages for use by the kernel,
+    // following the sbrk region.  Record this for later.
+    dma_addr64 first_dma = usable_regions[0].first;
+    dma_addr64 last_dma  = usable_regions[-1].last + 1;
+
+    // Remove everything above the top bootloader address since those pages,
+    // while free, aren't currently mapped.
+    region_remove(usable_regions,top_addr,0xFFFFFFFFFFFFFFFF);
 
     // We are kind of relying on the fact that sbrk() can allocate a
     // contiguous range without there being any holes in the memory map.
@@ -138,21 +70,24 @@ kernel::page_preinit(const e820_map* m, uint64_t top_addr)
     kassert(usable_regions[0].first == (uintptr_t)get_sbrk_limit());
     kassert(usable_regions[0].last == top_addr - 1);
 
-    // Populate the free page list.
-    populate_pages(usable_regions,free_page_list);
+    // Reserve memory for the buddy allocator bitmap.
+    size_t len  = last_dma - first_dma;
+    auto params = buddy_allocator_params(first_dma,len);
+    void* bitmap = phys_to_virt(sbrk(params.get_inuse_bitmask_size()));
+    buddy_pages.init(first_dma,len,bitmap);
 
-    // Reset sbrk since we are done with the two pages backing our local
-    // vectors.
-    kassert(sbrk(0) == initial_sbrk + 8192);
-    set_sbrk(initial_sbrk);
+    // Populate the buddy allocator.
+    uint64_t begin_pfn = ceil_div(usable_regions[0].first,PAGE_SIZE);
+    uint64_t end_pfn   = floor_div(usable_regions[0].last+1,PAGE_SIZE);
+    for (uint64_t pfn = begin_pfn; pfn != end_pfn; ++pfn)
+        buddy_pages->free_pages(pfn*PAGE_SIZE,0);
 }
 
 void
 kernel::page_init(const e820_map* m, uint64_t top_addr)
 {
-    // Map all of the remaining pages not initially mapped by the bootloader.
-    // This will be all pages past the first 8M of memory, mapped in the
-    // 0xFFFF800000000000 region.
+    // Map all of the remaining pages not initially mapped by the bootloader
+    // (i.e. the pages not handled in page_preinit()).
 
     // Build up the list of free regions, removing everything below the top
     // bootloader address.
@@ -163,8 +98,8 @@ kernel::page_init(const e820_map* m, uint64_t top_addr)
     regions_remove(free_regions,unusable_regions);
     region_remove(free_regions,0,top_addr-1);
 
-    // Now we are going to map all of the free RAM into the 0xFFFF800000000000
-    // area.
+    // Now we are going to map all of the remaining free RAM into the
+    // 0xFFFF800000000000 area.
     bool gp = (get_current_cpu()->flags & CPU_FLAG_PAGESIZE_1G);
     for (auto& r : free_regions)
     {
@@ -194,10 +129,7 @@ kernel::page_init(const e820_map* m, uint64_t top_addr)
                 kernel_task->pt.map_4k_page((void*)vaddr,paddr,PAGE_FLAGS_DATA);
                 npfns = 1;
             }
-
-            page* p = (page*)vaddr;
-            for (size_t i=0; i<npfns; ++i)
-                page_add(p++);
+            buddy_pages->free_pages(paddr,ulog2(npfns));
 
             pfn      += npfns;
             rem_pfns -= npfns;
