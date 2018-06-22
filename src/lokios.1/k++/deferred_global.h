d472191b2152fc9d54ce3bf0bdc664a9c1dbde70
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-06-22 02:24:35 +0000

    deferred_global: Add class to allow initialization of a global on demand.
    This wraps a block of memory to hold a global variable that will be
    constructed only when the global's init() method is invoked.  The
    deferred_global<> itself is a constexpr so it's backing memory is
    initialized at compile-time.

---
+++ src/lokios.1/k++/deferred_global.h
@@ -0,0 +1,52 @@
+#ifndef __KERNEL_DEFERRED_GLOBAL_H
+#define __KERNEL_DEFERRED_GLOBAL_H
+
+#include "hdr/compiler.h"
+#include <utility>
+
+namespace kernel
+{
+    template<typename T>
+    struct deferred_global
+    {
+        bool    inited;
+        char    storage[sizeof(T)] __ALIGNED__(alignof(T));
+
+        inline operator bool() {return inited;}
+
+        inline operator T&()
+        {
+            kassert(inited);
+            return *(T*)storage;
+        }
+
+        inline T* operator->()
+        {
+            kassert(inited);
+            return (T*)storage;
+        }
+
+        template<typename ...Args>
+        inline void init(Args&& ...args)
+        {
+            kassert(!inited);
+            inited = true;
+            new(storage) T(std::forward<Args>(args)...);
+        }
+
+        inline void destroy()
+        {
+            kassert(inited);
+            inited = false;
+            ((T*)storage)->~T();
+        }
+
+        constexpr deferred_global():
+            inited(false),
+            storage{}
+        {
+        }
+    };
+}
+
+#endif /* __KERNEL_DEFERRED_GLOBAL_H */
