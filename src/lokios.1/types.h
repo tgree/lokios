e8a6f6d010c974fcf166b2ef3a496895c7b79875
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-07-13 08:05:07 +0000

    types.h: Add 64-bit big- and little-endian types.

--- src/lokios.1/types.h
+++ src/lokios.1/types.h
@@ -29,6 +29,19 @@ namespace kernel
 template<typename T> T swap_uint(T);
 
 template<>
+constexpr uint64_t swap_uint<uint64_t>(uint64_t v)
+{
+    return ((v << 56) & 0xFF00000000000000) |
+           ((v << 40) & 0x00FF000000000000) |
+           ((v << 24) & 0x0000FF0000000000) |
+           ((v <<  8) & 0x000000FF00000000) |
+           ((v >>  8) & 0x00000000FF000000) |
+           ((v >> 24) & 0x0000000000FF0000) |
+           ((v >> 40) & 0x000000000000FF00) |
+           ((v >> 56) & 0x00000000000000FF);
+}
+
+template<>
 constexpr uint32_t swap_uint<uint32_t>(uint32_t v)
 {
     return (v << 24) | ((v << 8) & 0x00FF0000) |
@@ -46,10 +59,12 @@ template<typename T> constexpr T to_little_endian(T v)   {return v;}
 template<typename T> constexpr T from_little_endian(T v) {return v;}
 template<typename T> constexpr T to_big_endian(T v)      {return swap_uint(v);}
 template<typename T> constexpr T from_big_endian(T v)    {return swap_uint(v);}
+KASSERT(to_little_endian<uint64_t>(0x1122334455667788) == 0x1122334455667788);
 KASSERT(to_little_endian<uint32_t>(0x11223344)   == 0x11223344);
 KASSERT(to_little_endian<uint16_t>(0x1122)       == 0x1122);
 KASSERT(from_little_endian<uint32_t>(0x11223344) == 0x11223344);
 KASSERT(from_little_endian<uint16_t>(0x1122)     == 0x1122);
+KASSERT(to_big_endian<uint64_t>(0x1122334455667788) == 0x8877665544332211);
 KASSERT(to_big_endian<uint32_t>(0x11223344)      == 0x44332211);
 KASSERT(to_big_endian<uint16_t>(0x1122)          == 0x2211);
 KASSERT(from_big_endian<uint32_t>(0x11223344)    == 0x44332211);
@@ -59,10 +74,12 @@ template<typename T> constexpr T to_little_endian(T v)   {return swap_uint(v);}
 template<typename T> constexpr T from_little_endian(T v) {return swap_uint(v);}
 template<typename T> constexpr T to_big_endian(T v)      {return v;}
 template<typename T> constexpr T from_big_endian(T v)    {return v;}
+KASSERT(to_little_endian<uint64_t>(0x1122334455667788) == 0x8877665544332211);
 KASSERT(to_little_endian<uint32_t>(0x11223344)   == 0x44332211);
 KASSERT(to_little_endian<uint16_t>(0x1122)       == 0x2211);
 KASSERT(from_little_endian<uint32_t>(0x11223344) == 0x44332211);
 KASSERT(from_little_endian<uint16_t>(0x1122)     == 0x2211);
+KASSERT(to_big_endian<uint64_t>(0x1122334455667788) == 0x1122334455667788);
 KASSERT(to_big_endian<uint32_t>(0x11223344)      == 0x11223344);
 KASSERT(to_big_endian<uint16_t>(0x1122)          == 0x1122);
 KASSERT(from_big_endian<uint32_t>(0x11223344)    == 0x11223344);
@@ -89,8 +106,10 @@ struct le_uint
     inline le_uint() = default;
 } __PACKED__;
 
+typedef be_uint<uint64_t> be_uint64_t;
 typedef be_uint<uint32_t> be_uint32_t;
 typedef be_uint<uint16_t> be_uint16_t;
+typedef le_uint<uint64_t> le_uint64_t;
 typedef le_uint<uint32_t> le_uint32_t;
 typedef le_uint<uint16_t> le_uint16_t;
 
