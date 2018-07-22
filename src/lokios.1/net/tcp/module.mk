dddb0b6bf77d0e8bdfec0ccfcce1deba535a82e1
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-07-22 09:16:36 +0000

    tcp: Add a tcp.a module.
    This is empty for now, but we will start adding socket code soon.

--- src/lokios.1/net/tcp/module.mk
+++ src/lokios.1/net/tcp/module.mk
@@ -1,1 +1,11 @@
 SUBMODULES :=
+
+TCP_SRC := $(wildcard $(MODULE_SRC_DIR)/*.cc)
+TCP_OBJ := $(TCP_SRC:$(MODULE_SRC_DIR)/%.cc=$(MODULE_BUILD_DIR)/%.o)
+
+-include $(TCP_OBJ:.o=.d)
+
+$(LIB_DIR)/tcp.a: $(TCP_OBJ) $(MODULE_MK)
+	@echo Archive $@...
+	@mkdir -p $(LIB_DIR)
+	@$(AR) $(ARFLAGS) $@ $(TCP_OBJ)
