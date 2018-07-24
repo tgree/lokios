26cadf7b7225995341a8b470832b68035a029287
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-07-24 01:26:51 +0000

    make_all_fast: Do a 'make clean' before building.
    This starts us in a sane state and then does incremental builds from
    there.

--- docker/fs/home/userhome/make_all_fast.sh
+++ docker/fs/home/userhome/make_all_fast.sh
@@ -4,6 +4,7 @@
 # an interactive rebase and build them testing for build
 # and unittest successes.  It will stop on the first commit
 # that fails or go until the rebase is complete.
+make clean
 while true; do
     make -j || break
     qemu-system-x86_64 -drive file=bin/lokios.mbr,format=raw -nographic -device isa-debug-exit -smp 2 -acpitable sig=iTST,data=/dev/null
