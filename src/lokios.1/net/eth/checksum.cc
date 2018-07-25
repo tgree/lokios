2762af28b1cdd72943146f0655389c95e9d9eb50
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-07-25 07:59:15 +0000

    eth: Support tcp checksumming for software drivers.

--- src/lokios.1/net/eth/checksum.cc
+++ src/lokios.1/net/eth/checksum.cc
@@ -18,12 +18,7 @@ eth::insert_checksums(net::tx_op* op)
     auto* uhdr  = (udp::header*)(iphdr + 1);
     auto* thdr  = (tcp::header*)(iphdr + 1);
     if (op->flags & NTX_FLAG_INSERT_TCP_CSUM)
-    {
-        kassert(op->alps[0].len >= sizeof(eth::header) + sizeof(ipv4::header) +
-                                   sizeof(tcp::header));
-        kassert(thdr->checksum == 0);
-        kernel::panic("software tcp checksums not supported yet!");
-    }
+        thdr->checksum = tcp::compute_checksum(op,sizeof(eth::header));
     else if (op->flags & NTX_FLAG_INSERT_UDP_CSUM)
         uhdr->checksum = udp::compute_checksum(op,sizeof(eth::header));
     if (op->flags & NTX_FLAG_INSERT_IP_CSUM)
