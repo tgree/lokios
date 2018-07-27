d6fc348c003dce2fd40912cd6732b80ec7e7c193
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-07-27 06:38:58 +0000

    tmock: Improve assert_equiv to include file and line number.

--- src/tmock/tassert.cc
+++ src/tmock/tassert.cc
@@ -11,70 +11,80 @@ tmock::abort(const char* s)
 }
 
 void
-tmock::assert_equiv(const char* s, const char* expected)
+tmock::assert_equiv(const char* s, const char* expected, const char* file,
+    size_t line)
 {
     if (strcmp(s,expected))
     {
         if (!(tmock::internal::mode_flags & TMOCK_MODE_FLAG_SILENT))
         {
-            printf("Expected: '%s'\n",expected);
-            printf("     Got: '%s'\n",s);
+            printf("%s:%zu:\n",file,line);
+            printf(" Expected: '%s'\n",expected);
+            printf("      Got: '%s'\n",s);
         }
         exit(-1);
     }
 }
 
 void
-tmock::assert_equiv(uint16_t v, uint16_t expected)
+tmock::assert_equiv(uint16_t v, uint16_t expected, const char* file,
+    size_t line)
 {
     if (v != expected)
     {
         if (!(tmock::internal::mode_flags & TMOCK_MODE_FLAG_SILENT))
         {
-            printf("Expected: 0x%04X\n",expected);
-            printf("     Got: 0x%04X\n",v);
+            printf("%s:%zu:\n",file,line);
+            printf(" Expected: 0x%04X\n",expected);
+            printf("      Got: 0x%04X\n",v);
         }
         exit(-1);
     }
 }
 
 void
-tmock::assert_equiv(uint64_t v, uint64_t expected)
+tmock::assert_equiv(uint64_t v, uint64_t expected, const char* file,
+    size_t line)
 {
     if (v != expected)
     {
         if (!(tmock::internal::mode_flags & TMOCK_MODE_FLAG_SILENT))
         {
-            printf("Expected: 0x%016lX\n",expected);
-            printf("     Got: 0x%016lX\n",v);
+            printf("%s:%zu:\n",file,line);
+            printf(" Expected: 0x%016lX\n",expected);
+            printf("      Got: 0x%016lX\n",v);
         }
         exit(-1);
     }
 }
 
 void
-tmock::assert_equiv(int32_t v, int32_t expected)
+tmock::assert_equiv(int32_t v, int32_t expected, const char* file,
+    size_t line)
 {
     if (v != expected)
     {
         if (!(tmock::internal::mode_flags & TMOCK_MODE_FLAG_SILENT))
         {
-            printf("Expected: %u\n",expected);
-            printf("     Got: %u\n",v);
+            printf("%s:%zu:\n",file,line);
+            printf(" Expected: %u\n",expected);
+            printf("      Got: %u\n",v);
         }
         exit(-1);
     }
 }
 
 void
-tmock::assert_equiv(int64_t v, int64_t expected)
+tmock::assert_equiv(int64_t v, int64_t expected, const char* file,
+    size_t line)
 {
     if (v != expected)
     {
         if (!(tmock::internal::mode_flags & TMOCK_MODE_FLAG_SILENT))
         {
-            printf("Expected: %lu\n",expected);
-            printf("     Got: %lu\n",v);
+            printf("%s:%zu:\n",file,line);
+            printf(" Expected: %lu\n",expected);
+            printf("      Got: %lu\n",v);
         }
         exit(-1);
     }
--- src/tmock/tmock.h
+++ src/tmock/tmock.h
@@ -17,11 +17,21 @@ namespace tmock
     // Assert that two objects are equivalent.  For values it is just a simple
     // comparison with ==.  For things like const char* it will do a string
     // comparison.
-    void assert_equiv(const char* s, const char* expected);
-    void assert_equiv(uint16_t v, uint16_t expected);
-    void assert_equiv(uint64_t v, uint64_t expected);
-    void assert_equiv(int32_t v, int32_t expected);
-    void assert_equiv(int64_t v, int64_t expected);
+    void assert_equiv(const char* s, const char* expected,
+                      const char* file = __builtin_FILE(),
+                      size_t line = __builtin_LINE());
+    void assert_equiv(uint16_t v, uint16_t expected,
+                      const char* file = __builtin_FILE(),
+                      size_t line = __builtin_LINE());
+    void assert_equiv(uint64_t v, uint64_t expected,
+                      const char* file = __builtin_FILE(),
+                      size_t line = __builtin_LINE());
+    void assert_equiv(int32_t v, int32_t expected,
+                      const char* file = __builtin_FILE(),
+                      size_t line = __builtin_LINE());
+    void assert_equiv(int64_t v, int64_t expected,
+                      const char* file = __builtin_FILE(),
+                      size_t line = __builtin_LINE());
 
     int run_tests(int argc, const char* argv[]);
 
