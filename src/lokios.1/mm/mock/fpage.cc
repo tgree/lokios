8178521cfc9a13ac62994de27c4e4881e57c3be1
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-07-28 00:52:58 +0000

    mm: Get fpage to validate that the vaddr will work as a paddr.
    There are restrictions in the x86 page tables on what can be used as a
    paddr - no CPU actually supports a full 64-bit paddr.  To avoid
    accidentally generating an illegal paddr we check that the vaddr could
    map to a valid paddr before actually stuffing it in a fake page table or
    something.

--- src/lokios.1/mm/mock/fpage.cc
+++ src/lokios.1/mm/mock/fpage.cc
@@ -1,4 +1,5 @@
 #include "../page.h"
+#include "../page_flags.h"
 #include <stdlib.h>
 
 size_t page_alloc_count;
@@ -7,8 +8,12 @@ size_t page_free_count;
 void*
 kernel::page_alloc()
 {
+    // Allocate a 4K page that will work as a fake physical address (meaning
+    // that it has to have zero high-order bits according to what is supported
+    // by the x86 page table).
     void* p;
     kassert(posix_memalign(&p,PAGE_SIZE,PAGE_SIZE) == 0);
+    kassert(((uintptr_t)p & ~PAGE_PADDR_MASK) == 0);
     ++page_alloc_count;
     return p;
 }
