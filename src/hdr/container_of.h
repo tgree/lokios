9443aae80e4a715a496c452f3df9e61e89a51b6f
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-07-31 21:39:05 +0000

    container_of: Rename include guard for consistency with the rest of this directory.

--- src/hdr/container_of.h
+++ src/hdr/container_of.h
@@ -1,9 +1,9 @@
-#ifndef __KERNEL_CONTAINER_OF_H
-#define __KERNEL_CONTAINER_OF_H
+#ifndef __LOKIOS_CONTAINER_OF_H
+#define __LOKIOS_CONTAINER_OF_H
 
 #include <stddef.h>
 
 #define container_of(p,type,field) \
     ((type*)((char*)(1 ? (p) : &((type *)0)->field) - offsetof(type,field)))
 
-#endif /* __KERNEL_CONTAINER_OF_H */
+#endif /* __LOKIOS_CONTAINER_OF_H */
