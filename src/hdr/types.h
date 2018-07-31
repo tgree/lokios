d691e05266d62cee4a21782a0934edd13081df46
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-07-31 22:18:43 +0000

    types: Provide loki::remove_reference_t and remove_pointer_t.

--- src/hdr/types.h
+++ src/hdr/types.h
@@ -10,6 +10,17 @@ namespace loki
 #define container_of(p,type,field) \
         ((type*)((char*)(1 ? (p) : &((type *)0)->field) - offsetof(type,field)))
 
+    // Extract the underlying type from a reference parameter.
+    template<typename T> struct remove_reference      {typedef T type;};
+    template<typename T> struct remove_reference<T&>  {typedef T type;};
+    template<typename T> struct remove_reference<T&&> {typedef T type;};
+    template<typename T>
+    using remove_reference_t = typename remove_reference<T>::type;
+
+    // Extract the underlying type from a pointer parameter.
+    template<typename T>
+    using remove_pointer_t = remove_reference_t<decltype(*(T)0)>;
+
     // Type that cannot be copied if you subclass it.
     struct non_copyable
     {
