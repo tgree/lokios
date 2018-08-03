0801360d97f3c19508f137d48831588392620ec3
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-08-03 01:25:34 +0000

    compiler.h: Update for 16-bit support.
    When compiling for 16-bit (or 32-bit, presumably, since they're the
    same), gcc doesn't support uint128_t.  There are also some nice function
    attributes we can use to speed the code up a bit.

--- src/hdr/compiler.h
+++ src/hdr/compiler.h
@@ -6,10 +6,14 @@
 #define __ALIGNED__(n)  __attribute__((aligned(n)))
 #define __PRINTF__(a,b) __attribute__((format(printf,a,b)))
 #define __NORETURN__    __attribute__((noreturn))
+#define __FASTCALL__    __attribute__((fastcall))
+#define __REG_PARMS__   __attribute__((regparm(3)))
 
 #define __CACHE_ALIGNED__   __ALIGNED__(64)
 
+#if __SIZEOF_POINTER__ == 8
 typedef __uint128_t uint128_t;
+#endif
 
 #define __COMPILER_BARRIER__()  asm volatile("":::"memory")
 
