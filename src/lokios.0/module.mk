6eee8a670a81ec9b613a4128b67c8aa1a251b274
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-08-05 22:02:38 +0000

    lokios.0: Remove ability to have source code in the lokios.0 directory.
    Use sub-modules for goodness sake!  This directory no longer has any
    source code and it doesn't really make sense to have anything here;
    we're either building 32-bit code in the mode32 library or 16-bit code
    in the bootsector library.

--- src/lokios.0/module.mk
+++ src/lokios.0/module.mk
@@ -1,24 +1,13 @@
 SUBMODULES := bootsector mode32
 
-LOKIOS_0_CXX_SRC := $(wildcard $(MODULE_SRC_DIR)/*.cc)
-LOKIOS_0_ASM_SRC := $(wildcard $(MODULE_SRC_DIR)/*.s)
-
-LOKIOS_0_OBJ := $(LOKIOS_0_CXX_SRC:$(MODULE_SRC_DIR)/%.cc=$(MODULE_BUILD_DIR)/%.o) \
-                $(LOKIOS_0_ASM_SRC:$(MODULE_SRC_DIR)/%.s=$(MODULE_BUILD_DIR)/%.o)
-
-$(LOKIOS_0_OBJ): ASFLAGS := $(MODE16_ASFLAGS)
-$(LOKIOS_0_OBJ): KERN_CXXFLAGS := $(MODE16_CXXFLAGS)
-
 LOKIOS_0_LIB := bootsector.a mode32.a
 
 TIMESTAMP_S := $(MODULE_BUILD_DIR)/timestamp.s
 TIMESTAMP_O := $(MODULE_BUILD_DIR)/timestamp.o
 
--include $(LOKIOS_0_OBJ:.o=.d)
-
 $(MODULE_BUILD_DIR)/lokios.0.elf: LDM  := $(MODULE_BUILD_DIR)/lokios.0.map
 $(MODULE_BUILD_DIR)/lokios.0.elf: LDLD := $(MODULE_SRC_DIR)/lokios.0.ld
-$(MODULE_BUILD_DIR)/lokios.0.elf: $(LOKIOS_0_OBJ) $(LOKIOS_0_LIB:%=$(LIB_DIR)/%) $(MODULE_SRC_DIR)/lokios.0.ld $(MODULE_MK)
+$(MODULE_BUILD_DIR)/lokios.0.elf: $(LOKIOS_0_LIB:%=$(LIB_DIR)/%) $(MODULE_SRC_DIR)/lokios.0.ld $(MODULE_MK)
 	@echo '.data' > $(TIMESTAMP_S)
 	@echo '.globl _BUILD_BANNER' >> $(TIMESTAMP_S)
 	@echo '_BUILD_BANNER: .ascii "Loki\\r\\nCopyright (c) 2017-2018 by Terry Greeniaus.\\r\\n"' >> $(TIMESTAMP_S)
@@ -27,7 +16,6 @@ $(MODULE_BUILD_DIR)/lokios.0.elf: $(LOKIOS_0_OBJ) $(LOKIOS_0_LIB:%=$(LIB_DIR)/%)
 	@$(AS) $(MODE16_ASFLAGS) -o $(TIMESTAMP_O) $(TIMESTAMP_S)
 	@echo 'Linking $@...'
 	@ld -melf_i386 -Map=$(LDM) -T $(LDLD) --whole-archive -o $@ \
-	    $(LOKIOS_0_OBJ)                                         \
 	    $(TIMESTAMP_O)                                          \
 	    -\(                                                     \
 	    $(LOKIOS_0_LIB:%=$(LIB_DIR)/%)                          \
