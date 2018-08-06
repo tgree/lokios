e4b58231eac7815057d2b2ce84b746831bf65219
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-08-06 04:50:35 +0000

    mbr: Refactor mbr_read_sectors out of mbr_entry.
    We're separating the code that does the sector reading from the code
    that parses the read data.  In a future commit mode32.cc is going to be
    come responsible for interpreting it all.

--- src/lokios.0/mode32/mbr.cc
+++ src/lokios.0/mode32/mbr.cc
@@ -10,36 +10,20 @@ extern uint8_t _extra_sectors[];
 extern uint8_t _mbr_drive_number;
 extern uint8_t _pre_e820_bounce_buffer[];
 
-int
-mbr_entry()
+static int
+mbr_read_sectors(void* dst, uint16_t nsectors)
 {
-    uint32_t extra_sectors       = (uint32_t)_extra_sectors;
-    uint32_t first_kernel_sector = extra_sectors + 1;
-    uint8_t mbr_drive_number     = _mbr_drive_number;
-    uint32_t* buf                = (uint32_t*)(uint32_t)_pre_e820_bounce_buffer;
-    int err;
-
-    // Read the first sector.
-    err = disk_read(mbr_drive_number,first_kernel_sector,1,buf);
-    if (err)
-        return err;
-
-    // Validate the image header.
-    auto* khdr = (kernel::image_header*)buf;
-    if (khdr->sig != IMAGE_HEADER_SIG)
-    {
-        console::printf("Invalid kernel header signature.\n");
-        return -6;
-    }
+    // Global state.
+    static uint32_t sector_num = (uint32_t)_extra_sectors + 1;
 
     // Read all the sectors.
-    uint32_t rem_sectors = khdr->num_sectors;
-    uint32_t sector_num  = first_kernel_sector;
-    auto* pos            = (char*)_kernel_base;
+    uint32_t rem_sectors = nsectors;
+    auto* pos            = (char*)dst;
+    uint32_t* buf        = (uint32_t*)(uint32_t)_pre_e820_bounce_buffer;
     while (rem_sectors)
     {
         uint32_t nsectors = (rem_sectors >= 8 ? 8 : rem_sectors);
-        err = disk_read(mbr_drive_number,sector_num,nsectors,buf);
+        int err = disk_read(_mbr_drive_number,sector_num,nsectors,buf);
         if (err)
             return err;
 
@@ -48,6 +32,30 @@ mbr_entry()
         sector_num  += nsectors;
         pos         += 512*nsectors;
     }
+
+    return 0;
+}
+
+int
+mbr_entry()
+{
+    // Read the first sector into kernel_base.
+    auto* khdr = (kernel::image_header*)(uint32_t)_kernel_base;
+    int err = mbr_read_sectors(khdr,1);
+    if (err)
+        return err;
+
+    // Validate the image header.
+    if (khdr->sig != IMAGE_HEADER_SIG)
+    {
+        console::printf("Invalid kernel header signature.\n");
+        return -6;
+    }
+
+    // Read the remaining sectors.
+    err = mbr_read_sectors((char*)khdr + 512,khdr->num_sectors-1);
+    if (err)
+        return err;
 
     console::printf("MBR entry complete.\n");
     return 0;
