f9bbf09b3fdf76d41b26f6f4b87aab0abb2d6d1a
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-08-06 07:01:02 +0000

    elf: Add dumb elf image handling.
    If the elf image is meant to be loaded at 2M and all of the sections are
    fully populated in the image (i.e. none that need zero-filling by the
    loader) then we can load the elf image in place and execute it.
    
    Coincidentally, this is exactly how the lokios.1.elf image is crafted.
    I wonder how that happened?

--- src/lokios.0/mode32/elf_image.cc
+++ src/lokios.0/mode32/elf_image.cc
@@ -1,7 +1,44 @@
 #include "elf_image.h"
+#include "console.h"
+#include "kernel/image.h"
+#include <string.h>
+
+extern uint8_t _elf_base[];
+extern uint8_t _kernel_base[];
 
 int
 process_elf_image(image_stream* is, elf_header* sector0)
 {
-    return -1;
+    console::printf("Processing elf kernel image.\n");
+
+    // Copy the first sector into place.
+    auto* ehdr = (elf_header*)_elf_base;
+    memcpy(ehdr,sector0,512);
+
+    // Read sectors until we have the kernel header.
+    uint16_t nsectors = (_kernel_base - _elf_base)/512;
+    int err = is->read((char*)ehdr + 512,nsectors);
+    if (err)
+    {
+        console::printf("Error %d reading up to kernel header sector.\n",err);
+        return err;
+    }
+
+    // Sanity.
+    auto* khdr = (kernel::image_header*)_kernel_base;
+    if (khdr->sig != IMAGE_HEADER_SIG)
+    {
+        console::printf("ELF file was not a lokios kernel.\n");
+        return -1;
+    }
+
+    // Read the remaining sectors.
+    err = is->read((char*)khdr + 512,khdr->num_sectors-1);
+    if (err)
+    {
+        console::printf("Error %d reading remaining sectors.\n",err);
+        return err;
+    }
+
+    return 0;
 }
