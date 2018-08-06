1d3aa3a09dcbc40b9ae8a6ee15aea61bddbfcc16
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-08-06 03:59:13 +0000

    lokios.1: Add a footer to the kernel image.
    This will allow us to validate that we've got the whole thing.

--- src/lokios.1/image.h
+++ src/lokios.1/image.h
@@ -8,6 +8,13 @@ namespace kernel
         uint32_t    num_sectors;
         uint32_t    page_table_addr;
     };
+
+#define IMAGE_FOOTER_SIG    0x4C4F4B49 // 'IKOL'
+    struct image_footer
+    {
+        uint8_t     dd[508];
+        uint32_t    sig;
+    };
 }
 
 #endif /* __KERNEL_IMAGE_H */
