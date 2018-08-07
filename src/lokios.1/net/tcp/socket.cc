0c01c70b6189a78dfcc9a6f5fcba8165b554afce
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-08-07 23:41:48 +0000

    tcp: Provide a stubbed-out socket class.

---
+++ src/lokios.1/net/tcp/socket.cc
@@ -0,0 +1,33 @@
+#include "socket.h"
+#include "traits.h"
+#include "net/interface.h"
+
+tcp::socket::socket(net::interface* intf, uint16_t port):
+    intf(intf),
+    state(TCP_LISTEN),
+    prev_state(TCP_CLOSED)
+{
+#if 0
+    hdrs.ll.dst_mac          = eth::net_traits::zero_addr;
+    hdrs.ll.src_mac          = intf->hw_mac;
+    hdrs.ll.ether_type       = ipv4::net_traits::ether_type;
+#endif
+    hdrs.ip.version_ihl      = 0x45;
+    hdrs.ip.dscp_ecn         = 0;
+    hdrs.ip.total_len        = sizeof(ipv4::header) + sizeof(tcp::header);
+    hdrs.ip.identification   = 0;
+    hdrs.ip.flags_fragoffset = 0x4000;
+    hdrs.ip.ttl              = 64;
+    hdrs.ip.proto            = tcp::net_traits::ip_proto;
+    hdrs.ip.header_checksum  = 0;
+    hdrs.ip.src_ip           = intf->ip_addr;
+    hdrs.ip.dst_ip           = ipv4::addr{0,0,0,0};
+    hdrs.tcp.src_port        = port;
+    hdrs.tcp.dst_port        = 0;
+    hdrs.tcp.seq_num         = 0;
+    hdrs.tcp.ack_num         = 0;
+    hdrs.tcp.flags_offset    = 0;
+    hdrs.tcp.window_size     = 0;
+    hdrs.tcp.checksum        = 0;
+    hdrs.tcp.urgent_pointer  = 0;
+}
--- src/lokios.1/net/tcp/socket.h
+++ src/lokios.1/net/tcp/socket.h
@@ -26,6 +26,23 @@ namespace tcp
     };
     typedef typeof_field(listener,should_accept)   should_accept_delegate;
     typedef typeof_field(listener,socket_accepted) socket_accepted_delegate;
+
+    struct socket
+    {
+        enum tcp_state
+        {
+            TCP_CLOSED,
+            TCP_LISTEN,
+        };
+
+        net::interface*                 intf;
+        tcp::ll_ipv4_tcp_headers        hdrs;
+        tcp_state                       state;
+        tcp_state                       prev_state;
+
+        // Passive open.
+        socket(net::interface* intf, uint16_t port);
+    };
 }
 
 #endif /* __KERNEL_NET_TCP_SOCKET_H */
