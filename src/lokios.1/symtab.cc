b321891cf93529784a602bd0d500e9b73deaf46f
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-08-07 06:30:23 +0000

    symtab: Add support for elf symbol lookup.

--- src/lokios.1/symtab.cc
+++ src/lokios.1/symtab.cc
@@ -1,12 +1,24 @@
 #include "symtab.h"
+#include "elf_image.h"
+#include "console.h"
+#include "k++/deferred_init.h"
+
+using kernel::console::printf;
+
+extern uint8_t _virt_elf_header[];
+static kernel::deferred_init<elf64::image> elf;
 
 kernel::sym_info
 kernel::get_sym_info(void* addr)
 {
-    throw symbol_not_found_exception();
+    if (!elf || !elf->symtab)
+        throw symbol_not_found_exception();
+    return elf->get_sym_info(addr);
 }
 
 void
 kernel::init_symtab()
 {
+    if (*(uint32_t*)_virt_elf_header == 0x464C457F)
+        elf.init(_virt_elf_header);
 }
