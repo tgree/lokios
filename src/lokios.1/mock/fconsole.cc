e1bb1326d53e384d0a9a446163df681f6b38977d
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-08-13 23:05:10 +0000

    fconsole: Add missing v2printf function.

--- src/lokios.1/mock/fconsole.cc
+++ src/lokios.1/mock/fconsole.cc
@@ -12,3 +12,14 @@ kernel::console::vprintf(const char* fmt, va_list ap)
 
     ::vprintf(fmt,ap);
 }
+
+void
+kernel::console::v2printf(const char* fmt1, va_list ap1, const char* fmt2,
+    va_list ap2)
+{
+    if (fconsole_suppress_output)
+        return;
+
+    ::vprintf(fmt1,ap1);
+    ::vprintf(fmt2,ap2);
+}
