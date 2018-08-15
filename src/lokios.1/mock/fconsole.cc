391d70685f654ddcd2dd63dc0ece2eff43c46093
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-08-15 05:09:19 +0000

    fconsole: Implement kernel::console::hexdump().

--- src/lokios.1/mock/fconsole.cc
+++ src/lokios.1/mock/fconsole.cc
@@ -23,3 +23,63 @@ kernel::console::v2printf(const char* fmt1, va_list ap1, const char* fmt2,
     ::vprintf(fmt1,ap1);
     ::vprintf(fmt2,ap2);
 }
+
+
+static const char isprint_table[] =
+    "................................ !\"#$%&'()*+,-./0123456789:;<=>?"
+    "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~."
+    "................................................................"
+    "................................................................";
+
+void
+kernel::console::hexdump(const void* addr, size_t len, unsigned long base)
+{
+    if (fconsole_suppress_output)
+        return;
+
+    const unsigned char* p = (const unsigned char*)addr;
+    while (len >= 16)
+    {
+        printf("%08lX: %02X %02X %02X %02X  %02X %02X %02X %02X  "
+               "%02X %02X %02X %02X  %02X %02X %02X %02X "
+               "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",
+               base,p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8],p[9],p[10],
+               p[11],p[12],p[13],p[14],p[15],
+               isprint_table[p[0]],
+               isprint_table[p[1]],
+               isprint_table[p[2]],
+               isprint_table[p[3]],
+               isprint_table[p[4]],
+               isprint_table[p[5]],
+               isprint_table[p[6]],
+               isprint_table[p[7]],
+               isprint_table[p[8]],
+               isprint_table[p[9]],
+               isprint_table[p[10]],
+               isprint_table[p[11]],
+               isprint_table[p[12]],
+               isprint_table[p[13]],
+               isprint_table[p[14]],
+               isprint_table[p[15]]
+               );
+        base += 16;
+        p    += 16;
+        len  -= 16;
+    }
+    if (!len)
+        return;
+
+    printf("%08lX:",base);
+    for (size_t i=0; i<16; ++i)
+    {
+        if (i < len)
+            printf(" %02X",p[i]);
+        else
+            printf("   ");
+        if (i % 4 == 3)
+            printf(" ");
+    }
+    for (size_t i=0; i<len; ++i)
+        printf("%c",isprint_table[p[i]]);
+    printf("\n");
+}
