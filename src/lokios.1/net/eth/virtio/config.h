e90921b4aa8cd810f24c6e114ccf5aeab4ab7a91
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-08-20 04:35:48 +0000

    virtio: Provide macros for the TX and RX MTUs.

--- src/lokios.1/net/eth/virtio/config.h
+++ src/lokios.1/net/eth/virtio/config.h
@@ -4,4 +4,10 @@
 // Whether or not to hexdump packets to the kernel console on the TX path.
 #define VIRTIO_NET_DUMP_TX_PACKETS  0
 
+// The receive MTU.
+#define VIRTIO_NET_RX_MTU           1500
+
+// The transmit MTU.
+#define VIRTIO_NET_TX_MTU           1500
+
 #endif /* __KERNEL_NET_VIRTIO_CONFIG_H */
