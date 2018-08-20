6a4cad755f6c5261dd49d34189e3f52621745158
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-08-20 08:37:08 +0000

    docker: Move make_all scripts into new tools/ directory.
    These are generic scripts; they can be use to iterate over the commits
    in any git repository.  However, having them at the root of the
    filesystem instead of in the lokios.git directory means they aren't
    easily version-controlled which is kind of a pain.

---
+++ tools/make_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Helper script to iterate through all "edit" commits in
+# an interactive rebase and build them testing for build
+# and unittest successes.  It will stop on the first commit
+# that fails or go until the rebase is complete.
+while true; do
+    make clean && make -j || break
+    qemu-system-x86_64 -cpu qemu64,+popcnt -drive file=bin/lokios.elf.mbr,format=raw -nographic -device isa-debug-exit -smp 2 -acpitable sig=iTST,data=/dev/null -device virtio-net-pci,netdev=net0,disable-legacy=on,disable-modern=off,vectors=4 -netdev user,id=net0,hostfwd=tcp::12345-:12345 -object filter-dump,id=dump0,netdev=net0,file=net0dump.pcap -m 64M
+    if [ $? -ne 3 ]
+    then
+        echo "-----Integration test failure-----"
+        break
+    fi
+    git rebase --continue || break
+done
