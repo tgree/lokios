38eefca1012e8c82fa2bcc54186d2471f0dffb2f
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-08-26 07:33:04 +0000

    wip

--- src/lokios.0/mode32/massert.cc
+++ src/lokios.0/mode32/massert.cc
@@ -1,6 +1,7 @@
 #include "massert.h"
 #include "console.h"
 #include "hdr/x86.h"
+#include "kernel/kassert.h"
 
 extern "C"
 {
@@ -16,5 +17,11 @@ void
 _abort(const char* f, int l)
 {
     console::printf("ASSERTION FAILED: %s:%u\n",f,l);
+    abort();
+}
+
+void
+kernel::panic(const char* s, const char* f, unsigned int l) noexcept
+{
     abort();
 }
