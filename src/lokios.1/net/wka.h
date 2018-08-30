cf92c7f8f449805addf7fddd9fd005fbe13328d2
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-08-30 07:01:47 +0000

    net: Add well-known-addresses header.

---
+++ src/lokios.1/net/wka.h
@@ -0,0 +1,10 @@
+#ifndef __KERNEL_NET_WKA_H
+#define __KERNEL_NET_WKA_H
+
+// List of well-known addresses.
+#define TCP_HTTP_PORT           80
+
+// List of LokiOS-specific well-known addresses.
+#define TCP_LOKIOS_CMD_PORT     12345
+
+#endif /* __KERNEL_NET_WKA_H */
