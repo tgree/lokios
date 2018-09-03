245b1e6c33a010b999e816bdefa6638579dfad53
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-09-03 10:56:22 +0000

    fbuddy_allocator: Provide alloc and free counts for unittesting.

--- src/lokios.1/mm/mock/fbuddy_allocator.cc
+++ src/lokios.1/mm/mock/fbuddy_allocator.cc
@@ -1,11 +1,15 @@
 #include "../buddy_allocator.h"
 #include <stdlib.h>
 
+size_t buddy_alloc_count;
+size_t buddy_free_count;
+
 dma_addr64
 kernel::buddy_palloc(size_t order)
 {
     void* p;
     kassert(posix_memalign(&p,(PAGE_SIZE << order),(PAGE_SIZE << order)) == 0);
+    ++buddy_alloc_count;
     return virt_to_phys(p);
 }
 
@@ -14,6 +18,7 @@ kernel::buddy_pfree(dma_addr64 d, size_t order)
 {
     kassert(((uintptr_t)d & ((1<<(order+12))-1)) == 0);
     free(phys_to_virt(d));
+    ++buddy_free_count;
 }
 
 size_t
