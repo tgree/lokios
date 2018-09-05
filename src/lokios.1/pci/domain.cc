77ea244f378544cd753dc21b7494859e358495a8
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-09-05 05:25:28 +0000

    pci: Provide wapi nodes for pci domain objects.
    This provides the "/pci/0000" type of path.

---
+++ src/lokios.1/pci/domain.cc
@@ -0,0 +1,33 @@
+#include "domain.h"
+#include "pci.h"
+
+static void
+pci_domain_request(wapi::node* node, http::request* req, json::object* obj,
+    http::response* rsp)
+{
+    // GET /pci/0000
+    auto* d = container_of(node,kernel::pci::domain,wapi_node);
+    rsp->printf("{\r\n"
+                "    \"devices\" : [");
+    for (auto& pd : klist_elems(d->devices,domain_link))
+    {
+        rsp->printf("\r\n        { "
+                    "\"slot\" : \"%04X:%02X:%u.%u\", "
+                    "\"vendor_id\" : \"%04X\", "
+                    "\"device_id\" : \"%04X\", "
+                    "\"driver\" : \"%s\" "
+                    "},",
+                    pd.domain->id,pd.bus,(pd.devfn >> 3),(pd.devfn & 7),
+                    pd.config_read_16(0),pd.config_read_16(2),pd.owner->name);
+    }
+    rsp->ks.shrink();
+    rsp->printf("\r\n    ]\r\n}\r\n");
+}
+
+kernel::pci::domain::domain(uint16_t id, config_accessor* cfg):
+    id(id),
+    cfg(cfg),
+    wapi_node(func_delegate(pci_domain_request),METHOD_GET_MASK,"%04X",id)
+{
+    kernel::pci::wapi_node.register_child(&wapi_node);
+}
--- src/lokios.1/pci/domain.h
+++ src/lokios.1/pci/domain.h
@@ -2,6 +2,7 @@
 #define __KERNEL_PCI_DOMAIN_H
 
 #include "cfg.h"
+#include "wapi/wapi.h"
 #include "k++/klist.h"
 
 namespace kernel::pci
@@ -13,8 +14,9 @@ namespace kernel::pci
         uint16_t                        id;
         struct config_accessor*         cfg;
         kernel::klist<kernel::pci::dev> devices;
+        wapi::node                      wapi_node;
 
-        domain(uint16_t id, config_accessor* cfg):id(id),cfg(cfg) {}
+        domain(uint16_t id, config_accessor* cfg);
     };
 
     static inline bool operator<(const domain& lhs, const domain& rhs)
