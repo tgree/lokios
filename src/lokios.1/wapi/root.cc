33c5726b2ceca52104d0f2e43282867f16b39e7a
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-09-06 10:27:56 +0000

    wapi: Allow setting the / state to rebooting.

--- src/lokios.1/wapi/root.cc
+++ src/lokios.1/wapi/root.cc
@@ -21,16 +21,29 @@ root_set_state_stopped(tcp::send_op*)
 }
 
 static void
+root_set_state_rebooting(tcp::send_op*)
+{
+    kernel::reboot_guest();
+}
+
+static void
 root_post(json::object& obj, http::response* rsp)
 {
-    bool should_stop = false;
+    enum
+    {
+        ACTION_NONE,
+        ACTION_STOP,
+        ACTION_REBOOT,
+    } action = ACTION_NONE;
 
     for (auto& n : obj)
     {
         if (!strcmp(n.k,"state"))
         {
             if (!strcmp(n.v,"stopped"))
-                should_stop = true;
+                action = ACTION_STOP;
+            else if (!strcmp(n.v,"rebooting"))
+                action = ACTION_REBOOT;
             else if (strcmp(n.v,"running"))
                 throw wapi::bad_request_exception();
         }
@@ -38,8 +51,19 @@ root_post(json::object& obj, http::response* rsp)
             throw wapi::bad_request_exception();
     }
 
-    if (should_stop)
-        rsp->send_comp_delegate = func_delegate(root_set_state_stopped);
+    switch (action)
+    {
+        case ACTION_NONE:
+        break;
+
+        case ACTION_STOP:
+            rsp->send_comp_delegate = func_delegate(root_set_state_stopped);
+        break;
+
+        case ACTION_REBOOT:
+            rsp->send_comp_delegate = func_delegate(root_set_state_rebooting);
+        break;
+    }
 }
 
 static void
