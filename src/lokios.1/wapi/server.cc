d11084ebb0b1f3d970ab55fa7d98c7bc119f206f
Terry Greeniaus <terrygreeniaus@gmail.com>
2018-09-07 06:53:48 +0000

    wapi: More refactoring of the server class.

--- src/lokios.1/wapi/server.cc
+++ src/lokios.1/wapi/server.cc
@@ -14,6 +14,7 @@ struct wapi_connection : public tcp::socket_observer
 
     virtual void    socket_established(tcp::socket* s);
     virtual void    socket_readable(tcp::socket* s);
+            void    handle_request();
             void    response_send_complete(tcp::send_op* sop);
     virtual void    socket_recv_closed(tcp::socket* s);
     virtual void    socket_closed(tcp::socket* s);
@@ -65,40 +66,6 @@ wapi_connection::socket_readable(tcp::socket* s)
             size_t n    = request.parse(start,p->client_len);
             s->skip(n);
         }
-        if (!request.is_done())
-            return;
-
-        // Try to find a node to handle this request.
-        auto* n = wapi::find_node_for_path(request.request_target);
-        if (!n)
-            throw wapi::not_found_exception();
-        if (!(n->method_mask & (1<<request.method)))
-            throw wapi::method_not_allowed_exception(n->method_mask);
-
-        // Handle it.
-        if (request.is_json_request())
-        {
-            json::object obj;
-            json::parse_object(request.body.c_str(),&obj);
-            n->handler(n,&request,&obj,&response);
-        }
-        else
-            n->handler(n,&request,NULL,&response);
-
-        // Success path.
-        response.headerf("HTTP/1.1 200 OK\r\n"
-                         "Content-Type: application/json\r\n"
-                         "Content-Length: %zu\r\n"
-                         "\r\n",
-                         response.ks.strlen());
-    }
-    catch (const http::missing_content_type_exception&)
-    {
-        // This indicates that there was a request body but no Content-Type
-        // field to tell us how to interpret it.  Recoverable.
-        response.headerf("HTTP/1.1 400 Bad Request\r\n"
-                         "Content-Length: 0\r\n"
-                         "\r\n");
     }
     catch (const http::exception&)
     {
@@ -114,56 +81,101 @@ wapi_connection::socket_readable(tcp::socket* s)
         s->close_send();
         return;
     }
-    catch (const json::exception&)
-    {
-        // We couldn't parse the JSON body.  The stream is recoverable even if
-        // the JSON is gibberish.
-        response.headerf("HTTP/1.1 400 Bad Request\r\n"
-                         "Content-Length: 0\r\n"
-                         "\r\n");
-    }
-    catch (const wapi::not_found_exception&)
-    {
-        // Node not found.  Recoverable.
-        response.headerf("HTTP/1.1 404 Not Found\r\n"
-                         "Content-Length: 15\r\n"
-                         "\r\n"
-                         "404 Not Found\r\n");
-    }
-    catch (const wapi::bad_request_exception&)
-    {
-        // Something was wrong at the wapi level.  Examples include setting the
-        // root '/' node json 'state' field to an unrecognized value or issuing
-        // a POST without an expected JSON body.  Recoverable.
-        response.headerf("HTTP/1.1 400 Bad Request\r\n"
-                         "Content-Length: 0\r\n"
-                         "\r\n");
-    }
-    catch (const wapi::method_not_allowed_exception& e)
-    {
-        // This node doesn't support the requested method.  Recoverable.
-        response.headerf("HTTP/1.1 405 Method Not Allowed\r\n"
-                         "Content-Length: 0\r\n"
-                         "Allow: ");
-        uint64_t methods = e.method_mask;
-        for (size_t i=0; i<http::NUM_METHODS; ++i)
-        {
-            bool supported = (methods & 1);
-            methods      >>= 1;
-            if (supported)
-            {
-                response.headerf("%s",http::get_method_name((http::method)i));
-                if (methods)
-                    response.headerf(", ");
-            }
-        }
-        response.headerf("\r\n\r\n");
-    }
 
-    // Send the final response, be it a recoverable error or success.
+    // If we haven't finished parsing (more to come from the remote client),
+    // then bail out now.
+    if (!request.is_done())
+        return;
+
+    // Generate the response.  At this point we have properly parsed the
+    // request, so the stream is always recoverable.  handle_request will
+    // generate whatever type of response we should send and then we'll send
+    // it.
+    handle_request();
     response.send(s)->cookie = s;
     if (!request.should_keepalive())
         s->close_send();
+}
+
+void
+wapi_connection::handle_request() try
+{
+    // Try to find a node to handle this request.
+    auto* n = wapi::find_node_for_path(request.request_target);
+    if (!n)
+        throw wapi::not_found_exception();
+    if (!(n->method_mask & (1<<request.method)))
+        throw wapi::method_not_allowed_exception(n->method_mask);
+
+    // Dispatch it.
+    if (request.is_json_request())
+    {
+        json::object obj;
+        json::parse_object(request.body.c_str(),&obj);
+        n->handler(n,&request,&obj,&response);
+    }
+    else
+        n->handler(n,&request,NULL,&response);
+
+    // Success path.
+    response.headerf("HTTP/1.1 200 OK\r\n"
+                     "Content-Type: application/json\r\n"
+                     "Content-Length: %zu\r\n"
+                     "\r\n",
+                     response.ks.strlen());
+}
+catch (const http::missing_content_type_exception&)
+{
+    // This indicates that there was a request body but no Content-Type
+    // field to tell us how to interpret it.  Recoverable.
+    response.headerf("HTTP/1.1 400 Bad Request\r\n"
+                     "Content-Length: 0\r\n"
+                     "\r\n");
+}
+catch (const json::exception&)
+{
+    // We couldn't parse the JSON body.  The stream is recoverable even if
+    // the JSON is gibberish.
+    response.headerf("HTTP/1.1 400 Bad Request\r\n"
+                     "Content-Length: 0\r\n"
+                     "\r\n");
+}
+catch (const wapi::not_found_exception&)
+{
+    // Node not found.  Recoverable.
+    response.headerf("HTTP/1.1 404 Not Found\r\n"
+                     "Content-Length: 15\r\n"
+                     "\r\n"
+                     "404 Not Found\r\n");
+}
+catch (const wapi::bad_request_exception&)
+{
+    // Something was wrong at the wapi level.  Examples include setting the
+    // root '/' node json 'state' field to an unrecognized value or issuing
+    // a POST without an expected JSON body.  Recoverable.
+    response.headerf("HTTP/1.1 400 Bad Request\r\n"
+                     "Content-Length: 0\r\n"
+                     "\r\n");
+}
+catch (const wapi::method_not_allowed_exception& e)
+{
+    // This node doesn't support the requested method.  Recoverable.
+    response.headerf("HTTP/1.1 405 Method Not Allowed\r\n"
+                     "Content-Length: 0\r\n"
+                     "Allow: ");
+    uint64_t methods = e.method_mask;
+    for (size_t i=0; i<http::NUM_METHODS; ++i)
+    {
+        bool supported = (methods & 1);
+        methods      >>= 1;
+        if (supported)
+        {
+            response.headerf("%s",http::get_method_name((http::method)i));
+            if (methods)
+                response.headerf(", ");
+        }
+    }
+    response.headerf("\r\n\r\n");
 }
 
 void
